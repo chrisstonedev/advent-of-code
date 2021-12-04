548b0c7d37be73052d8165e150994553dcbf330c
Chris Stone <contact@chrisstone.dev>
2021-12-04 00:31:33 -0500

    Changed Kotlin project structure

--- kotlin/build.gradle.kts
+++ kotlin/build.gradle.kts
@@ -1,29 +1,15 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
 plugins {
     kotlin("jvm") version "1.5.10"
-    application
 }
-
-group = "dev.chrisstone"
-version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
 }
 
-dependencies {
-    testImplementation(kotlin("test"))
-}
-
-tasks.test {
-    useJUnitPlatform()
-}
-
-tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "1.8"
-}
-
-application {
-    mainClass.set("MainKt")
+tasks {
+    sourceSets {
+        main {
+            java.srcDirs("src")
+        }
+    }
 }
--- kotlin/src/Utils.kt
+++ kotlin/src/Utils.kt
@@ -5,9 +5,9 @@ import java.security.MessageDigest
 /**
  * Reads lines from the given input txt file.
  */
-fun readInput(name: String) = File("src/main/kotlin", "$name.txt").readLines()
+fun readInput(name: String) = File("src", "$name.txt").readLines()
 
-fun readInputAsInts(name: String) = File("src/main/kotlin", "$name.txt").readLines().map { it.toInt() }
+fun readInputAsInts(name: String) = readInput(name).map { it.toInt() }
 
 /**
  * Converts string to md5 hash.
