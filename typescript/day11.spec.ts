bb846c88c16bf032d2aa4bd8fab61abfeea23e09
Chris Stone <contact@chrisstone.dev>
2022-02-03 17:06:36 -0500

    Part 1 of Day 11 complete in TypeScript

--- typescript/day11.spec.ts
+++ typescript/day11.spec.ts
@@ -6,7 +6,7 @@ describe('Day 11', () => {
   it('should increase energy level by 1 each step', () => {
     let initial = [[1]];
     let expected = [[2]];
-    let actual = day.advanceSteps(initial, 1);
+    let actual = day.advanceSteps(initial, 1).energyLevelGrid;
 
     expect(actual).toEqual(expected);
   });
@@ -14,7 +14,7 @@ describe('Day 11', () => {
   it('should set level to 0 after it flashes', () => {
     let initial = [[9]];
     let expected = [[0]];
-    let actual = day.advanceSteps(initial, 1);
+    let actual = day.advanceSteps(initial, 1).energyLevelGrid;
 
     expect(actual).toEqual(expected);
   });
@@ -22,7 +22,7 @@ describe('Day 11', () => {
   it('should increase adjacent energy levels of a flashing octopus', () => {
     let initial = [[1, 1, 1], [1, 9, 1], [1, 1, 1]];
     let expected = [[3, 3, 3], [3, 0, 3], [3, 3, 3]];
-    let actual = day.advanceSteps(initial, 1);
+    let actual = day.advanceSteps(initial, 1).energyLevelGrid;
 
     expect(actual).toEqual(expected);
   });
@@ -30,7 +30,7 @@ describe('Day 11', () => {
   it('should increase adjacent energy levels when flashing without touching others', () => {
     let initial = [[1, 1, 1, 1], [1, 9, 1, 1], [1, 1, 1, 1], [1, 1, 1, 1]];
     let expected = [[3, 3, 3, 2], [3, 0, 3, 2], [3, 3, 3, 2], [2, 2, 2, 2]];
-    let actual = day.advanceSteps(initial, 1);
+    let actual = day.advanceSteps(initial, 1).energyLevelGrid;
 
     expect(actual).toEqual(expected);
   });
@@ -38,7 +38,7 @@ describe('Day 11', () => {
   it('should handle multiple flashing octopuses', () => {
     let initial = [[1, 1, 1, 1], [1, 9, 9, 1], [1, 1, 1, 1], [1, 1, 1, 1]];
     let expected = [[3, 4, 4, 3], [3, 0, 0, 3], [3, 4, 4, 3], [2, 2, 2, 2]];
-    let actual = day.advanceSteps(initial, 1);
+    let actual = day.advanceSteps(initial, 1).energyLevelGrid;
 
     expect(actual).toEqual(expected);
   });
@@ -46,16 +46,63 @@ describe('Day 11', () => {
   it('should handle the first step of the complex example', () => {
     let initial = [[1, 1, 1, 1, 1], [1, 9, 9, 9, 1], [1, 9, 1, 9, 1], [1, 9, 9, 9, 1], [1, 1, 1, 1, 1]];
     let expected = [[3, 4, 5, 4, 3], [4, 0, 0, 0, 4], [5, 0, 0, 0, 5], [4, 0, 0, 0, 4], [3, 4, 5, 4, 3]];
-    let actual = day.advanceSteps(initial, 1);
+    let actual = day.advanceSteps(initial, 1).energyLevelGrid;
 
     expect(actual).toEqual(expected);
   });
 
-  it('should handle the second step of the complex example', () => {
-    let initial = [[1, 1, 1, 1, 1], [1, 9, 9, 9, 1], [1, 9, 1, 9, 1], [1, 9, 9, 9, 1], [1, 1, 1, 1, 1]];
-    let expected = [[4, 5, 6, 5, 4], [5, 1, 1, 1, 5], [6, 1, 1, 1, 6], [5, 1, 1, 1, 5], [4, 5, 6, 5, 4]];
-    let actual = day.advanceSteps(initial, 2);
+  describe('Using the test input file', () => {
+    let initial = [
+      [5, 4, 8, 3, 1, 4, 3, 2, 2, 3],
+      [2, 7, 4, 5, 8, 5, 4, 7, 1, 1],
+      [5, 2, 6, 4, 5, 5, 6, 1, 7, 3],
+      [6, 1, 4, 1, 3, 3, 6, 1, 4, 6],
+      [6, 3, 5, 7, 3, 8, 5, 4, 7, 8],
+      [4, 1, 6, 7, 5, 2, 4, 6, 4, 5],
+      [2, 1, 7, 6, 8, 4, 1, 7, 2, 1],
+      [6, 8, 8, 2, 8, 8, 1, 1, 3, 4],
+      [4, 8, 4, 6, 8, 4, 8, 5, 5, 4],
+      [5, 2, 8, 3, 7, 5, 1, 5, 2, 6]];
 
-    expect(actual).toEqual(expected);
+    it('should give the right output for the test input after 2 steps', () => {
+      let expectedGrid = [
+        [8, 8, 0, 7, 4, 7, 6, 5, 5, 5],
+        [5, 0, 8, 9, 0, 8, 7, 0, 5, 4],
+        [8, 5, 9, 7, 8, 8, 9, 6, 0, 8],
+        [8, 4, 8, 5, 7, 6, 9, 6, 0, 0],
+        [8, 7, 0, 0, 9, 0, 8, 8, 0, 0],
+        [6, 6, 0, 0, 0, 8, 8, 9, 8, 9],
+        [6, 8, 0, 0, 0, 0, 5, 9, 4, 3],
+        [0, 0, 0, 0, 0, 0, 7, 4, 5, 6],
+        [9, 0, 0, 0, 0, 0, 0, 8, 7, 6],
+        [8, 7, 0, 0, 0, 0, 6, 8, 4, 8]];
+      let actual = day.advanceSteps(initial, 2);
+
+      expect(actual.energyLevelGrid).toEqual(expectedGrid);
+      expect(actual.totalFlashes).toEqual(35);
+    });
+
+    it('should give the right output for the test input after 3 steps', () => {
+      let expectedGrid = [
+        [0, 0, 5, 0, 9, 0, 0, 8, 6, 6],
+        [8, 5, 0, 0, 8, 0, 0, 5, 7, 5],
+        [9, 9, 0, 0, 0, 0, 0, 0, 3, 9],
+        [9, 7, 0, 0, 0, 0, 0, 0, 4, 1],
+        [9, 9, 3, 5, 0, 8, 0, 0, 6, 3],
+        [7, 7, 1, 2, 3, 0, 0, 0, 0, 0],
+        [7, 9, 1, 1, 2, 5, 0, 0, 0, 9],
+        [2, 2, 1, 1, 1, 3, 0, 0, 0, 0],
+        [0, 4, 2, 1, 1, 2, 5, 0, 0, 0],
+        [0, 0, 2, 1, 1, 1, 9, 0, 0, 0]];
+      let actual = day.advanceSteps(initial, 3);
+
+      expect(actual.energyLevelGrid).toEqual(expectedGrid);
+      expect(actual.totalFlashes).toEqual(80);
+    });
+
+    it('should give the right output for the test input after 10 steps', () => {
+      let actual = day.advanceSteps(initial, 10);
+      expect(actual.totalFlashes).toEqual(204);
+    });
   });
 });
--- typescript/day11.ts
+++ typescript/day11.ts
@@ -8,8 +8,8 @@ export class Day11 implements Day {
 
   executePartOne(input: string[]): number {
     let energyLevelGrid = input.map(x => x.split('').map(y => +y));
-    energyLevelGrid = this.advanceSteps(energyLevelGrid, 1);
-    return energyLevelGrid.length;
+    let result = this.advanceSteps(energyLevelGrid, 100);
+    return result.totalFlashes;
   }
 
   executePartTwo(input: string[]): number {
@@ -19,27 +19,36 @@ export class Day11 implements Day {
   public advanceSteps(energyLevelGrid: number[][], steps: number) {
     function advanceOneStep(initialGrid: number[][]) {
       let newGrid = initialGrid.map(x => x.map(y => y + 1));
-      for (let row = 0; row < newGrid.length; row++) {
-        for (let col = 0; col < newGrid.length; col++) {
-          if (newGrid[row][col] > 9) {
-            row > 0 && col > 0 && newGrid[row - 1][col - 1]++;
-            row > 0 && newGrid[row - 1][col]++;
-            row > 0 && col < newGrid.length - 1 && newGrid[row - 1][col + 1]++;
-            col > 0 && newGrid[row][col - 1]++;
-            col < newGrid.length - 1 && newGrid[row][col + 1]++;
-            row < newGrid.length - 1 && col > 0 && newGrid[row + 1][col - 1]++;
-            row < newGrid.length - 1 && newGrid[row + 1][col]++;
-            row < newGrid.length - 1 && col < newGrid.length - 1 && newGrid[row + 1][col + 1]++;
+      let flashes = 0;
+
+      while (newGrid.flat().filter(x => x > 9).length > 0) {
+        for (let row = 0; row < newGrid.length; row++) {
+          for (let col = 0; col < newGrid.length; col++) {
+            if (newGrid[row][col] > 9) {
+              flashes++;
+              newGrid[row][col] = -9999;
+              row > 0 && col > 0 && newGrid[row - 1][col - 1]++;
+              row > 0 && newGrid[row - 1][col]++;
+              row > 0 && col < newGrid.length - 1 && newGrid[row - 1][col + 1]++;
+              col > 0 && newGrid[row][col - 1]++;
+              col < newGrid.length - 1 && newGrid[row][col + 1]++;
+              row < newGrid.length - 1 && col > 0 && newGrid[row + 1][col - 1]++;
+              row < newGrid.length - 1 && newGrid[row + 1][col]++;
+              row < newGrid.length - 1 && col < newGrid.length - 1 && newGrid[row + 1][col + 1]++;
+            }
           }
         }
       }
-      newGrid = newGrid.map(x => x.map(y => y > 9 ? 0 : y));
-      return newGrid;
+      newGrid = newGrid.map(x => x.map(y => y < 0 ? 0 : y));
+      return {flashes, energyLevelGrid: newGrid};
     }
 
+    let totalFlashes = 0;
     for (let step = 0; step < steps; step++) {
-      energyLevelGrid = advanceOneStep(energyLevelGrid);
+      let result = advanceOneStep(energyLevelGrid);
+      totalFlashes += result.flashes;
+      energyLevelGrid = result.energyLevelGrid;
     }
-    return energyLevelGrid;
+    return {totalFlashes, energyLevelGrid};
   }
 }
