ae341d904953b114fa9d8e3f58d02f9b536cff6d
Chris Stone <contact@chrisstone.dev>
2022-02-03 17:26:50 -0500

    Changed 3 loops to 1 for Day 11!

--- typescript/day11.ts
+++ typescript/day11.ts
@@ -19,36 +19,28 @@ export class Day11 implements Day {
 
   private static advanceOneStep(initialGrid: number[][]) {
     let newGrid = initialGrid.map(x => x.map(y => y + 1));
-    let flashes = 0;
 
     while (newGrid.flat().filter(x => x > 9).length > 0) {
-      for (let row = 0; row < newGrid.length; row++) {
-        for (let col = 0; col < newGrid.length; col++) {
-          if (newGrid[row][col] > 9) {
-            flashes++;
-            newGrid[row][col] = -9999;
-            row > 0 && col > 0 && newGrid[row - 1][col - 1]++;
-            row > 0 && newGrid[row - 1][col]++;
-            row > 0 && col < newGrid.length - 1 && newGrid[row - 1][col + 1]++;
-            col > 0 && newGrid[row][col - 1]++;
-            col < newGrid.length - 1 && newGrid[row][col + 1]++;
-            row < newGrid.length - 1 && col > 0 && newGrid[row + 1][col - 1]++;
-            row < newGrid.length - 1 && newGrid[row + 1][col]++;
-            row < newGrid.length - 1 && col < newGrid.length - 1 && newGrid[row + 1][col + 1]++;
-          }
-        }
-      }
+      let row = newGrid.findIndex(x => x.filter(x => x > 9).length > 0);
+      let col = newGrid[row].findIndex(x => x > 9);
+      newGrid[row][col] = -9999;
+      row > 0 && col > 0 && newGrid[row - 1][col - 1]++;
+      row > 0 && newGrid[row - 1][col]++;
+      row > 0 && col < newGrid.length - 1 && newGrid[row - 1][col + 1]++;
+      col > 0 && newGrid[row][col - 1]++;
+      col < newGrid.length - 1 && newGrid[row][col + 1]++;
+      row < newGrid.length - 1 && col > 0 && newGrid[row + 1][col - 1]++;
+      row < newGrid.length - 1 && newGrid[row + 1][col]++;
+      row < newGrid.length - 1 && col < newGrid.length - 1 && newGrid[row + 1][col + 1]++;
     }
-    newGrid = newGrid.map(x => x.map(y => y < 0 ? 0 : y));
-    return {flashes, energyLevelGrid: newGrid};
+    return newGrid.map(x => x.map(y => y < 0 ? 0 : y));
   }
 
   public advanceSteps(energyLevelGrid: number[][], steps: number) {
     let totalFlashes = 0;
     for (let step = 0; step < steps; step++) {
-      let result = Day11.advanceOneStep(energyLevelGrid);
-      totalFlashes += result.flashes;
-      energyLevelGrid = result.energyLevelGrid;
+      energyLevelGrid = Day11.advanceOneStep(energyLevelGrid);
+      totalFlashes += energyLevelGrid.flat().filter(x => x === 0).length;
     }
     return {totalFlashes, energyLevelGrid};
   }
@@ -56,7 +48,7 @@ export class Day11 implements Day {
   private static advanceStepsUntilSynchronization(energyLevelGrid: number[][]) {
     let totalSteps = 0;
     while (energyLevelGrid.flat().filter(x => x !== 0).length > 0) {
-      energyLevelGrid = Day11.advanceOneStep(energyLevelGrid).energyLevelGrid;
+      energyLevelGrid = Day11.advanceOneStep(energyLevelGrid);
       totalSteps++;
     }
     return totalSteps;
