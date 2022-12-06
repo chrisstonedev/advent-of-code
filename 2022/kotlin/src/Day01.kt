1df1274ee731a719e7bb6a395b5e686d3106ac26
Chris Stone <contact@chrisstone.dev>
2022-12-06 02:42:46 -0500

    2022 Day 1 in Kotlin: Compact solution

--- 2022/kotlin/src/Day01.kt
+++ 2022/kotlin/src/Day01.kt
@@ -1,23 +1,12 @@
 fun main() {
-    fun getCalorieSumPerElf(input: String): List<Int> {
-        val data = input.split("\n\n").map { elf ->
-            elf.lines().sumOf { it.toInt() }
-        }
-        return data
-    }
+    fun parseInputData(input: String): List<List<Int>> =
+        input.split("\n\n").map { elf -> elf.lines().map { it.toInt() } }
 
-    fun getSumOfCaloriesFromTopNElves(input: String, topNElves: Int): Int {
-        val data = getCalorieSumPerElf(input)
-        return data.sortedDescending().take(topNElves).sum()
-    }
+    fun List<List<Int>>.topNElves(n: Int): Int = map { it.sum() }.sortedDescending().take(n).sum()
 
-    fun part1(input: String): Int {
-        return getSumOfCaloriesFromTopNElves(input, 1)
-    }
+    fun part1(input: String): Int = parseInputData(input).topNElves(1)
 
-    fun part2(input: String): Int {
-        return getSumOfCaloriesFromTopNElves(input, 3)
-    }
+    fun part2(input: String): Int = parseInputData(input).topNElves(3)
 
     val testInput = readInputAsText("Day01_test")
     checkValue(part1(testInput), 24000)
