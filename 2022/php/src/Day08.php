63f692b42aeb0594fe3922715ad3d1ff25077b13
Chris Stone <contact@chrisstone.dev>
2022-12-08 01:01:04 -0500

    2022 Day 8 in PHP: Combined functionality for both parts in private methods with method wrappers to define how the values are consumed

--- 2022/php/src/Day08.php
+++ 2022/php/src/Day08.php
@@ -47,91 +47,87 @@ class Day08
 
     private static function isVisibleFromTheTop(array $input, int $row, int $col): bool
     {
-        for ($rowAbove = 0; $rowAbove < $row; $rowAbove++) {
-            if (intval(substr($input[$rowAbove], $col, 1)) >= intval(substr($input[$row], $col, 1))) {
-                return false;
-            }
-        }
-        return true;
+        return self::getDistanceWithVisibilityFromTheTop($input, $row, $col) > 0;
     }
 
     private static function isVisibleFromTheBottom(array $input, int $row, int $col): bool
     {
-        for ($rowBelow = count($input) - 1; $rowBelow > $row; $rowBelow--) {
-            if (intval(substr($input[$rowBelow], $col, 1)) >= intval(substr($input[$row], $col, 1))) {
-                return false;
-            }
-        }
-        return true;
+        return self::getDistanceWithVisibilityFromTheBottom($input, $row, $col) > 0;
     }
 
     private static function isVisibleFromTheLeft(string $rowData, int $col): bool
     {
-        $isVisible = true;
-        for ($colBefore = 0; $colBefore < $col; $colBefore++) {
-            if (intval(substr($rowData, $colBefore, 1)) >= intval(substr($rowData, $col, 1))) {
-                $isVisible = false;
-                break;
-            }
-        }
-        return $isVisible;
+        return self::getDistanceWithVisibilityFromTheLeft($rowData, $col) > 0;
     }
 
     private static function isVisibleFromTheRight(string $rowData, int $col): bool
     {
-        $isVisible = true;
-        for ($colAfter = strlen($rowData) - 1; $colAfter > $col; $colAfter--) {
-            if (intval(substr($rowData, $colAfter, 1)) >= intval(substr($rowData, $col, 1))) {
-                $isVisible = false;
-                break;
-            }
-        }
-        return $isVisible;
+        return self::getDistanceWithVisibilityFromTheRight($rowData, $col) > 0;
     }
 
     private static function getDistanceFromTheTop(array $input, int $row, int $col): int
+    {
+        return abs(self::getDistanceWithVisibilityFromTheTop($input, $row, $col));
+    }
+
+    private static function getDistanceFromTheBottom(array $input, int $row, int $col): int
+    {
+        return abs(self::getDistanceWithVisibilityFromTheBottom($input, $row, $col));
+    }
+
+    private static function getDistanceFromTheLeft(string $rowData, int $col): int
+    {
+        return abs(self::getDistanceWithVisibilityFromTheLeft($rowData, $col));
+    }
+
+    private static function getDistanceFromTheRight(string $rowData, int $col): int
+    {
+        return abs(self::getDistanceWithVisibilityFromTheRight($rowData, $col));
+    }
+
+    private static function getDistanceWithVisibilityFromTheTop(array $input, int $row, int $col): int
     {
         $distance = 0;
         for ($rowAbove = $row - 1; $rowAbove >= 0; $rowAbove--) {
             $distance++;
             if (intval(substr($input[$rowAbove], $col, 1)) >= intval(substr($input[$row], $col, 1))) {
-                break;
+                return $distance * -1;
             }
         }
         return $distance;
     }
 
-    private static function getDistanceFromTheBottom(array $input, int $row, int $col): int
+    private static function getDistanceWithVisibilityFromTheBottom(array $input, int $row, int $col): int
     {
         $distance = 0;
         for ($rowBelow = $row + 1; $rowBelow < count($input); $rowBelow++) {
             $distance++;
             if (intval(substr($input[$rowBelow], $col, 1)) >= intval(substr($input[$row], $col, 1))) {
-                break;
+                return $distance * -1;
             }
         }
         return $distance;
     }
 
-    private static function getDistanceFromTheLeft(string $rowData, int $col): int
+    private static function getDistanceWithVisibilityFromTheLeft(string $rowData, int $col): int
     {
         $distance = 0;
         for ($colBefore = $col - 1; $colBefore >= 0; $colBefore--) {
             $distance++;
             if (intval(substr($rowData, $colBefore, 1)) >= intval(substr($rowData, $col, 1))) {
-                break;
+                return $distance * -1;
             }
         }
         return $distance;
     }
 
-    private static function getDistanceFromTheRight(string $rowData, int $col): int
+    private static function getDistanceWithVisibilityFromTheRight(string $rowData, int $col): int
     {
         $distance = 0;
         for ($colAfter = $col + 1; $colAfter < strlen($rowData); $colAfter++) {
             $distance++;
             if (intval(substr($rowData, $colAfter, 1)) >= intval(substr($rowData, $col, 1))) {
-                break;
+                return $distance * -1;
             }
         }
         return $distance;
