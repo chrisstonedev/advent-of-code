2d0a2b6382ec2da5f11198da9be635bfe26a9892
Chris Stone <contact@chrisstone.dev>
2022-12-08 00:45:03 -0500

    2022 Day 8 in PHP: Refactored inner loops into methods with clearer names

--- 2022/php/src/Day08.php
+++ 2022/php/src/Day08.php
@@ -11,54 +11,12 @@ class Day08
         $count = 0;
         for ($row = 0; $row < count($input); $row++) {
             for ($col = 0; $col < strlen($input[$row]); $col++) {
-                if ($row === 0 || $row === count($input) - 1 || $col === 0 || $col === strlen($input[$row]) - 1) {
+                if (self::isOnTheEdgeOfTheGrid($input, $row, $col) ||
+                    self::isVisibleFromTheTop($input, $row, $col) ||
+                    self::isVisibleFromTheBottom($input, $row, $col) ||
+                    self::isVisibleFromTheLeft($input[$row], $col) ||
+                    self::isVisibleFromTheRight($input[$row], $col)) {
                     $count++;
-                    continue;
-                }
-
-                $isVisible = true;
-                for ($rowAbove = 0; $rowAbove < $row; $rowAbove++) {
-                    if (intval(substr($input[$rowAbove], $col, 1)) >= intval(substr($input[$row], $col, 1))) {
-                        $isVisible = false;
-                        break;
-                    }
-                }
-                if ($isVisible) {
-                    $count++;
-                    continue;
-                }
-                $isVisible = true;
-                for ($rowBelow = count($input) - 1; $rowBelow > $row; $rowBelow--) {
-                    if (intval(substr($input[$rowBelow], $col, 1)) >= intval(substr($input[$row], $col, 1))) {
-                        $isVisible = false;
-                        break;
-                    }
-                }
-                if ($isVisible) {
-                    $count++;
-                    continue;
-                }
-                $isVisible = true;
-                for ($colBefore = 0; $colBefore < $col; $colBefore++) {
-                    if (intval(substr($input[$row], $colBefore, 1)) >= intval(substr($input[$row], $col, 1))) {
-                        $isVisible = false;
-                        break;
-                    }
-                }
-                if ($isVisible) {
-                    $count++;
-                    continue;
-                }
-                $isVisible = true;
-                for ($colAfter = count($input) - 1; $colAfter > $col; $colAfter--) {
-                    if (intval(substr($input[$row], $colAfter, 1)) >= intval(substr($input[$row], $col, 1))) {
-                        $isVisible = false;
-                        break;
-                    }
-                }
-                if ($isVisible) {
-                    $count++;
-                    continue;
                 }
             }
         }
@@ -68,50 +26,114 @@ class Day08
     public static function executePartTwo(array $input): int
     {
         $highestScenicScore = 0;
-        for ($row = 0; $row < count($input); $row++) {
-            for ($col = 0; $col < strlen($input[$row]); $col++) {
-                if ($row === 0 || $row === count($input) - 1 || $col === 0 || $col === strlen($input[$row]) - 1) {
-                    continue;
-                }
-
-                $scenicScore = 0;
-                $distance = 0;
-                for ($rowAbove = $row - 1; $rowAbove >= 0; $rowAbove--) {
-                    $distance++;
-                    if (intval(substr($input[$rowAbove], $col, 1)) >= intval(substr($input[$row], $col, 1))) {
-                        break;
-                    }
-                }
-                $scenicScore = $distance;
-                $distance = 0;
-                for ($rowBelow = $row + 1; $rowBelow < count($input); $rowBelow++) {
-                    $distance++;
-                    if (intval(substr($input[$rowBelow], $col, 1)) >= intval(substr($input[$row], $col, 1))) {
-                        break;
-                    }
-                }
-                $scenicScore *= $distance;
-                $distance = 0;
-                for ($colBefore = $col - 1; $colBefore >= 0; $colBefore--) {
-                    $distance++;
-                    if (intval(substr($input[$row], $colBefore, 1)) >= intval(substr($input[$row], $col, 1))) {
-                        break;
-                    }
-                }
-                $scenicScore *= $distance;
-                $distance = 0;
-                for ($colAfter = $col + 1; $colAfter < count($input); $colAfter++) {
-                    $distance++;
-                    if (intval(substr($input[$row], $colAfter, 1)) >= intval(substr($input[$row], $col, 1))) {
-                        break;
-                    }
-                }
-                $scenicScore *= $distance;
+        for ($row = 1; $row < count($input) - 1; $row++) {
+            for ($col = 1; $col < strlen($input[$row]) - 1; $col++) {
+                $scenicScore = self::getDistanceFromTheTop($input, $row, $col) *
+                    self::getDistanceFromTheBottom($input, $row, $col) *
+                    self::getDistanceFromTheLeft($input[$row], $col) *
+                    self::getDistanceFromTheRight($input[$row], $col);
                 if ($scenicScore > $highestScenicScore) {
                     $highestScenicScore = $scenicScore;
                 }
             }
         }
         return $highestScenicScore;
+    }
+
+    private static function isOnTheEdgeOfTheGrid(array $input, int $row, int $col): bool
+    {
+        return $row === 0 || $row === count($input) - 1 || $col === 0 || $col === strlen($input[$row]) - 1;
+    }
+
+    private static function isVisibleFromTheTop(array $input, int $row, int $col): bool
+    {
+        for ($rowAbove = 0; $rowAbove < $row; $rowAbove++) {
+            if (intval(substr($input[$rowAbove], $col, 1)) >= intval(substr($input[$row], $col, 1))) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static function isVisibleFromTheBottom(array $input, int $row, int $col): bool
+    {
+        for ($rowBelow = count($input) - 1; $rowBelow > $row; $rowBelow--) {
+            if (intval(substr($input[$rowBelow], $col, 1)) >= intval(substr($input[$row], $col, 1))) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static function isVisibleFromTheLeft(string $rowData, int $col): bool
+    {
+        $isVisible = true;
+        for ($colBefore = 0; $colBefore < $col; $colBefore++) {
+            if (intval(substr($rowData, $colBefore, 1)) >= intval(substr($rowData, $col, 1))) {
+                $isVisible = false;
+                break;
+            }
+        }
+        return $isVisible;
+    }
+
+    private static function isVisibleFromTheRight(string $rowData, int $col): bool
+    {
+        $isVisible = true;
+        for ($colAfter = strlen($rowData) - 1; $colAfter > $col; $colAfter--) {
+            if (intval(substr($rowData, $colAfter, 1)) >= intval(substr($rowData, $col, 1))) {
+                $isVisible = false;
+                break;
+            }
+        }
+        return $isVisible;
+    }
+
+    private static function getDistanceFromTheTop(array $input, int $row, int $col): int
+    {
+        $distance = 0;
+        for ($rowAbove = $row - 1; $rowAbove >= 0; $rowAbove--) {
+            $distance++;
+            if (intval(substr($input[$rowAbove], $col, 1)) >= intval(substr($input[$row], $col, 1))) {
+                break;
+            }
+        }
+        return $distance;
+    }
+
+    private static function getDistanceFromTheBottom(array $input, int $row, int $col): int
+    {
+        $distance = 0;
+        for ($rowBelow = $row + 1; $rowBelow < count($input); $rowBelow++) {
+            $distance++;
+            if (intval(substr($input[$rowBelow], $col, 1)) >= intval(substr($input[$row], $col, 1))) {
+                break;
+            }
+        }
+        return $distance;
+    }
+
+    private static function getDistanceFromTheLeft(string $rowData, int $col): int
+    {
+        $distance = 0;
+        for ($colBefore = $col - 1; $colBefore >= 0; $colBefore--) {
+            $distance++;
+            if (intval(substr($rowData, $colBefore, 1)) >= intval(substr($rowData, $col, 1))) {
+                break;
+            }
+        }
+        return $distance;
+    }
+
+    private static function getDistanceFromTheRight(string $rowData, int $col): int
+    {
+        $distance = 0;
+        for ($colAfter = $col + 1; $colAfter < strlen($rowData); $colAfter++) {
+            $distance++;
+            if (intval(substr($rowData, $colAfter, 1)) >= intval(substr($rowData, $col, 1))) {
+                break;
+            }
+        }
+        return $distance;
     }
 }
