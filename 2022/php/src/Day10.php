22c79fa735034d5f5219501e9eb6e796740fe989
Chris Stone <contact@chrisstone.dev>
2022-12-10 14:54:36 -0500

    2022 Day 10 Part 1 in PHP: Initial solution

--- 2022/php/src/Day10.php
+++ 2022/php/src/Day10.php
@@ -8,11 +8,33 @@ class Day10
 {
     public static function executePartOne(array $input): int
     {
-        return 1;
+        $x = 1;
+        $answer = 0;
+        $cycle = 1;
+        for ($index = 0; $index < count($input); $index++) {
+            $cycle++;
+            $answer += self::checkCycleAndAddToAnswer($cycle, $x);
+            $line = $input[$index];
+            if (strpos($line, 'addx') !== false) {
+                $value = intval(substr($line, 5));
+                $x += $value;
+                $cycle++;
+                $answer += self::checkCycleAndAddToAnswer($cycle, $x);
+            }
+        }
+        return $answer;
     }
 
     public static function executePartTwo(array $input): int
     {
+        return 0;
+    }
+
+    private static function checkCycleAndAddToAnswer(int $cycle, int $x)
+    {
+        if (($cycle + 20) % 40 === 0) {
+            return $cycle * $x;
+        }
         return 0;
     }
 }
