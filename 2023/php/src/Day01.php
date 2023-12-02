3926608de216a5e76772d7963556cd1f6adc8ea4
Chris Stone <contact@chrisstone.dev>
2023-12-02 15:37:23 -0500

    Refactoring some 2023 Day 1 in PHP

--- 2023/php/src/Day01.php
+++ 2023/php/src/Day01.php
@@ -6,6 +6,18 @@ namespace aoc2023;
 
 class Day01
 {
+    private static array $numberWords = [
+        'one' => '1',
+        'two' => '2',
+        'three' => '3',
+        'four' => '4',
+        'five' => '5',
+        'six' => '6',
+        'seven' => '7',
+        'eight' => '8',
+        'nine' => '9',
+    ];
+
     public static function executePartOne(array $input): int
     {
         $result = 0;
@@ -50,40 +62,9 @@ class Day01
                 $number = substr($line, $i, 1);
                 break;
             }
-            if (substr($line, $i, 3) === 'one') {
-                $number = '1';
-                break;
-            }
-            if (substr($line, $i, 3) === 'two') {
-                $number = '2';
-                break;
-            }
-            if (substr($line, $i, 5) === 'three') {
-                $number = '3';
-                break;
-            }
-            if (substr($line, $i, 4) === 'four') {
-                $number = '4';
-                break;
-            }
-            if (substr($line, $i, 4) === 'five') {
-                $number = '5';
-                break;
-            }
-            if (substr($line, $i, 3) === 'six') {
-                $number = '6';
-                break;
-            }
-            if (substr($line, $i, 5) === 'seven') {
-                $number = '7';
-                break;
-            }
-            if (substr($line, $i, 5) === 'eight') {
-                $number = '8';
-                break;
-            }
-            if (substr($line, $i, 4) === 'nine') {
-                $number = '9';
+            $numberFromStartOfString = self::getNumberFromStartOfString(substr($line, $i));
+            if ($numberFromStartOfString) {
+                $number = $numberFromStartOfString;
                 break;
             }
         }
@@ -92,43 +73,32 @@ class Day01
                 $number .= substr($line, $i, 1);
                 break;
             }
-            if (substr($line, $i, 3) === 'one') {
-                $number .= '1';
-                break;
-            }
-            if (substr($line, $i, 3) === 'two') {
-                $number .= '2';
-                break;
-            }
-            if (substr($line, $i, 5) === 'three') {
-                $number .= '3';
-                break;
-            }
-            if (substr($line, $i, 4) === 'four') {
-                $number .= '4';
-                break;
-            }
-            if (substr($line, $i, 4) === 'five') {
-                $number .= '5';
-                break;
-            }
-            if (substr($line, $i, 3) === 'six') {
-                $number .= '6';
-                break;
-            }
-            if (substr($line, $i, 5) === 'seven') {
-                $number .= '7';
-                break;
-            }
-            if (substr($line, $i, 5) === 'eight') {
-                $number .= '8';
-                break;
-            }
-            if (substr($line, $i, 4) === 'nine') {
-                $number .= '9';
+            $numberFromStartOfString = self::getNumberFromEndOfString(substr($line, 0, $i + 1));
+            if ($numberFromStartOfString) {
+                $number .= $numberFromStartOfString;
                 break;
             }
         }
         return intval($number);
+    }
+
+    private static function getNumberFromStartOfString(string $text): string|false
+    {
+        foreach (self::$numberWords as $word => $number) {
+            if (str_starts_with($text, $word)) {
+                return $number;
+            }
+        }
+        return false;
+    }
+
+    private static function getNumberFromEndOfString(string $text): string|false
+    {
+        foreach (self::$numberWords as $word => $number) {
+            if (str_ends_with($text, $word)) {
+                return $number;
+            }
+        }
+        return false;
     }
 }
