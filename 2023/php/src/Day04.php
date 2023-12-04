3159a244830ce8f7d0ecfdcb1bb540accc075e71
Chris Stone <contact@chrisstone.dev>
2023-12-04 00:30:07 -0500

    Improved readability of PHP 2023 Day 4

--- 2023/php/src/Day04.php
+++ 2023/php/src/Day04.php
@@ -10,12 +10,9 @@ class Day04
     {
         $value = 0;
         foreach ($input as $card) {
-            $numbers = explode(' | ', explode(': ', $card)[1]);
-            preg_match_all('/\d+/', $numbers[0], $winningNumbers);
-            preg_match_all('/\d+/', $numbers[1], $yourNumbers);
-            $test = array_intersect($winningNumbers[0], $yourNumbers[0]);
-            if (count($test) > 0) {
-                $value += pow(2, count($test) - 1);
+            $countOfWinningNumbers = self::getCountOfWinningNumbers($card);
+            if ($countOfWinningNumbers > 0) {
+                $value += pow(2, $countOfWinningNumbers - 1);
             }
         }
         return $value;
@@ -27,31 +24,33 @@ class Day04
         $repeats = [];
         foreach ($input as $index => $card) {
             $scratchcards++;
-            $numbers = explode(' | ', explode(': ', $card)[1]);
-            preg_match_all('/\d+/', $numbers[0], $winningNumbers);
-            preg_match_all('/\d+/', $numbers[1], $yourNumbers);
-            $test = array_intersect($winningNumbers[0], $yourNumbers[0]);
-            for ($i = 0; $i < count($test); $i++) {
+            $countOfWinningNumbers = self::getCountOfWinningNumbers($card);
+            for ($i = 0; $i < $countOfWinningNumbers; $i++) {
                 $repeats[] = $index + $i + 1;
             }
         }
         while ($repeats) {
-            $repeats = self::extracted($repeats, $input, $scratchcards);
+            $repeats = self::continueEvaluatingRepeatCards($repeats, $input, $scratchcards);
         }
         return $scratchcards;
     }
 
-    public static function extracted(array $repeats, array $input, int &$scratchcards): array
+    public static function getCountOfWinningNumbers(string $card): int
+    {
+        $numbers = explode(' | ', explode(': ', $card)[1]);
+        preg_match_all('/\d+/', $numbers[0], $winningNumbers);
+        preg_match_all('/\d+/', $numbers[1], $yourNumbers);
+        $winningNumbers = array_intersect($winningNumbers[0], $yourNumbers[0]);
+        return count($winningNumbers);
+    }
+
+    public static function continueEvaluatingRepeatCards(array $repeats, array $input, int &$scratchcards): array
     {
         $newRepeats = [];
         foreach ($repeats as $index) {
-            $card = $input[$index];
             $scratchcards++;
-            $numbers = explode(' | ', explode(': ', $card)[1]);
-            preg_match_all('/\d+/', $numbers[0], $winningNumbers);
-            preg_match_all('/\d+/', $numbers[1], $yourNumbers);
-            $test = array_intersect($winningNumbers[0], $yourNumbers[0]);
-            for ($i = 0; $i < count($test); $i++) {
+            $countOfWinningNumbers = self::getCountOfWinningNumbers($input[$index]);
+            for ($i = 0; $i < $countOfWinningNumbers; $i++) {
                 $newRepeats[] = $index + $i + 1;
             }
         }
