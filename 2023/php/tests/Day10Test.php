b0c8a43193d1893fc8210b6d8b393a2b83af7483
Chris Stone <contact@chrisstone.dev>
2023-12-11 15:09:13 -0500

    Cleaning up Day 10 in PHP

--- 2023/php/tests/Day10Test.php
+++ 2023/php/tests/Day10Test.php
@@ -20,9 +20,9 @@ class Day10Test extends TestCase
     public static function part1WithoutFilesProvider(): array
     {
         return [
-            'distance of 0' => [['S.'], 0],
-            'distance of 1' => [['S-'], 1],
-            'distance of 2' => [['S-7'], 2],
+            'distance of 0' => [['S.', '..'], 0],
+            'distance of 1' => [['S-', '|.'], 1],
+            'distance of 2' => [['S7', 'LJ'], 2],
         ];
     }
 
@@ -59,18 +59,50 @@ class Day10Test extends TestCase
     /** @dataProvider part2WithoutFilesProvider */
     public function testPart2WithoutFiles(array $input, int $expected)
     {
+        $inputCopy = $input;
+        $paths = Day::getPaths($inputCopy);
+        $enclosed = Day::getEnclosedThings($input, $paths);
+        print_r(Day::renderPrettyGrid($input, $paths, $enclosed));
+
         $this->assertSame($expected, Day::executePartTwo($input));
     }
 
     public static function part2WithoutFilesProvider(): array
     {
         return [
-            '3 inside box with no padding' => [['S---7','|...|','L---J'], 3],
-            '3 inside box with padding' => [['.S---7.','.|...|.','.L---J.'], 3],
-            'box with upper-right corner' => [['.S--7..','.|..L7.','.L---J.'], 2],
-            'box with lower-right corner' => [['.S---7.','.|..FJ.','.L--J..'], 2],
-            'box with upper-left corner' => [['..S--7.','.FJ..|.','.L---J.'], 2],
-            'box with lower-left corner' => [['.S---7.','.L7..|.','..L--J.'], 2],
+            '3 inside box with no padding' => [['S---7', '|...|', 'L---J'], 3],
+            '3 inside box with padding' => [['.S---7.', '.|...|.', '.L---J.'], 3],
+            'box with upper-right corner' => [['.S--7..', '.|..L7.', '.L---J.'], 2],
+            'box with lower-right corner' => [['.S---7.', '.|..FJ.', '.L--J..'], 2],
+            'box with upper-left corner' => [['..S--7.', '.FJ..|.', '.L---J.'], 2],
+            'box with lower-left corner' => [['.S---7.', '.L7..|.', '..L--J.'], 2],
+            'start in horizontal bar' => [['.F-S-7.', '.|...|.', '.L---J.'], 3],
+            'start in upper-right corner' => [['.F---S.', '.|...|.', '.L---J.'], 3],
+            'start in lower-left corner' => [['.F---7.', '.|...|.', '.S---J.'], 3],
+            'start in lower-right corner' => [['.F---7.', '.|...|.', '.L---S.'], 3],
+        ];
+    }
+
+    /** @dataProvider renderPrettyGridProvider */
+    public function testRenderPrettyGrid(array $input, array $expected)
+    {
+        $paths = Day::getPaths($input);
+        $this->assertSame(implode("\n", $expected), Day::renderPrettyGrid($input, $paths, null));
+    }
+
+    public static function renderPrettyGridProvider(): array
+    {
+        return [
+            '3 inside box with no padding' => [['S---7', '|...|', 'L---J'], ['┌───┐', '│   │', '└───┘']],
+            '3 inside box with padding' => [['.S---7.', '.|...|.', '.L---J.'], [' ┌───┐ ', ' │   │ ', ' └───┘ ']],
+            'box with upper-right corner' => [['.S--7..', '.|..L7.', '.L---J.'], [' ┌──┐  ', ' │  └┐ ', ' └───┘ ']],
+            'box with lower-right corner' => [['.S---7.', '.|..FJ.', '.L--J..'], [' ┌───┐ ', ' │  ┌┘ ', ' └──┘  ']],
+            'box with upper-left corner' => [['..S--7.', '.FJ..|.', '.L---J.'], ['  ┌──┐ ', ' ┌┘  │ ', ' └───┘ ']],
+            'box with lower-left corner' => [['.S---7.', '.L7..|.', '..L--J.'], [' ┌───┐ ', ' └┐  │ ', '  └──┘ ']],
+            'start in horizontal bar' => [['.F-S-7.', '.|...|.', '.L---J.'], [' ┌───┐ ', ' │   │ ', ' └───┘ ']],
+            'start in upper-right corner' => [['.F---S.', '.|...|.', '.L---J.'], [' ┌───┐ ', ' │   │ ', ' └───┘ ']],
+            'start in lower-left corner' => [['.F---7.', '.|...|.', '.S---J.'], [' ┌───┐ ', ' │   │ ', ' └───┘ ']],
+            'start in lower-right corner' => [['.F---7.', '.|...|.', '.L---S.'], [' ┌───┐ ', ' │   │ ', ' └───┘ ']],
         ];
     }
 }
