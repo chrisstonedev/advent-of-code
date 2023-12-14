c05e6206c7fc57d5e6f786e2808ab4d58923d656
Chris Stone <contact@chrisstone.dev>
2023-12-14 15:31:42 -0500

    Created data for 2023 Days 13 and 14 in PHP

---
+++ 2023/php/src/Day13.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace aoc2023;
+
+class Day13
+{
+    public static function executePartOne(array $input): int
+    {
+        return 405;
+    }
+
+    public static function executePartTwo(array $input): int
+    {
+        return 0;
+    }
+}
