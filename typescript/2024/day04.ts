e2eef9b2b89caba29cc80a0b221bfacc39712209
Chris Stone <chrisstonedev@gmail.com>
2024-12-04 23:11:05 -0500

    Refactored 2024 Day 4 in TypeScript

--- typescript/2024/day04.ts
+++ typescript/2024/day04.ts
@@ -9,55 +9,26 @@ export class Day04 implements Day {
     for (let row = 0; row < input.length; row++) {
       for (let col = 0; col < input[row].length; col++) {
         if (input[row][col] === "X") {
-          // let's check
-          const right = input[row].slice(col, col + 4);
-          if (right === "XMAS") {
-            // console.log("R\n");
-            counter++;
-          }
-          const left = input[row]
-            .slice(col - 3, col + 1)
-            .split("")
-            .reverse()
-            .join("");
-          if (left === "XMAS") {
-            // console.log("L\n");
-            counter++;
-          }
-          if (row <= input.length - 4) {
-            const down = `${input[row][col]}${input[row + 1][col]}${input[row + 2][col]}${input[row + 3][col]}`;
-            if (down === "XMAS") {
-              // console.log("D\n");
+          [
+            [0, 1],
+            [1, 1],
+            [1, 0],
+            [1, -1],
+            [0, -1],
+            [-1, -1],
+            [-1, 0],
+            [-1, 1],
+          ].forEach(([rowShift, colShift]) => {
+            if (
+              (rowShift <= 0 || row <= input.length - 4) &&
+              (rowShift >= 0 || row >= 3) &&
+              input[row + rowShift][col + colShift] === "M" &&
+              input[row + rowShift * 2][col + colShift * 2] === "A" &&
+              input[row + rowShift * 3][col + colShift * 3] === "S"
+            ) {
               counter++;
             }
-            const downleft = `${input[row][col]}${input[row + 1][col - 1]}${input[row + 2][col - 2]}${input[row + 3][col - 3]}`;
-            if (downleft === "XMAS") {
-              // console.log("DL\n");
-              counter++;
-            }
-            const downright = `${input[row][col]}${input[row + 1][col + 1]}${input[row + 2][col + 2]}${input[row + 3][col + 3]}`;
-            if (downright === "XMAS") {
-              // console.log("DR\n");
-              counter++;
-            }
-          }
-          if (row >= 3) {
-            const up = `${input[row][col]}${input[row - 1][col]}${input[row - 2][col]}${input[row - 3][col]}`;
-            if (up === "XMAS") {
-              // console.log("U\n");
-              counter++;
-            }
-            const upleft = `${input[row][col]}${input[row - 1][col - 1]}${input[row - 2][col - 2]}${input[row - 3][col - 3]}`;
-            if (upleft === "XMAS") {
-              // console.log("UL\n");
-              counter++;
-            }
-            const upright = `${input[row][col]}${input[row - 1][col + 1]}${input[row - 2][col + 2]}${input[row - 3][col + 3]}`;
-            if (upright === "XMAS") {
-              // console.log("UR\n");
-              counter++;
-            }
-          }
+          });
         }
       }
     }
@@ -69,36 +40,13 @@ export class Day04 implements Day {
     for (let row = 1; row < input.length - 1; row++) {
       for (let col = 1; col < input[row].length - 1; col++) {
         if (input[row][col] === "A") {
-          // let's check!
+          const bottomLeftToTopRight =
+            input[row + 1][col - 1] + input[row - 1][col + 1];
+          const upperLeftToBottomRight =
+            input[row - 1][col - 1] + input[row + 1][col + 1];
           if (
-            input[row - 1][col - 1] === "M" &&
-            input[row + 1][col + 1] === "S" &&
-            input[row + 1][col - 1] === "M" &&
-            input[row - 1][col + 1] === "S"
-          ) {
-            counter++;
-          }
-          if (
-            input[row - 1][col - 1] === "S" &&
-            input[row + 1][col + 1] === "M" &&
-            input[row + 1][col - 1] === "M" &&
-            input[row - 1][col + 1] === "S"
-          ) {
-            counter++;
-          }
-          if (
-            input[row - 1][col - 1] === "M" &&
-            input[row + 1][col + 1] === "S" &&
-            input[row + 1][col - 1] === "S" &&
-            input[row - 1][col + 1] === "M"
-          ) {
-            counter++;
-          }
-          if (
-            input[row - 1][col - 1] === "S" &&
-            input[row + 1][col + 1] === "M" &&
-            input[row + 1][col - 1] === "S" &&
-            input[row - 1][col + 1] === "M"
+            (bottomLeftToTopRight === "MS" || bottomLeftToTopRight === "SM") &&
+            (upperLeftToBottomRight === "MS" || upperLeftToBottomRight === "SM")
           ) {
             counter++;
           }
