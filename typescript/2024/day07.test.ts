f8ef95502ef48f3bff3498e4a78dd7d44cd7c52b
Chris Stone <chrisstonedev@gmail.com>
2024-12-07 01:41:50 -0500

    Refactoring day 7 2024 in TypeScript

--- typescript/2024/day07.test.ts
+++ typescript/2024/day07.test.ts
@@ -5,35 +5,39 @@ import { Day07 } from "./day07";
 describe("Day 7", () => {
   const day = new Day07();
 
-  [
-    { input: "190: 10 19", expected: true },
-    { input: "3267: 81 40 27", expected: true },
-    { input: "83: 17 5", expected: false },
-    { input: "156: 15 6", expected: false },
-    { input: "7290: 6 8 6 15", expected: false },
-    { input: "161011: 16 10 13", expected: false },
-    { input: "192: 17 8 14", expected: false },
-    { input: "21037: 9 7 18 13", expected: false },
-    { input: "292: 11 6 16 20", expected: true },
-  ].forEach(({ input, expected }) => {
-    it(`should return ${expected} from test input for ${JSON.stringify(input)}`, () => {
-      assert.deepStrictEqual(day.canBeValid(input), expected);
+  describe("when concat operator is not allowed", () => {
+    [
+      { input: "190: 10 19", expected: true },
+      { input: "3267: 81 40 27", expected: true },
+      { input: "83: 17 5", expected: false },
+      { input: "156: 15 6", expected: false },
+      { input: "7290: 6 8 6 15", expected: false },
+      { input: "161011: 16 10 13", expected: false },
+      { input: "192: 17 8 14", expected: false },
+      { input: "21037: 9 7 18 13", expected: false },
+      { input: "292: 11 6 16 20", expected: true },
+    ].forEach(({ input, expected }) => {
+      it(`should return ${expected} from test input for ${JSON.stringify(input)}`, () => {
+        assert.deepStrictEqual(day.canBeValid(input, false), expected);
+      });
     });
   });
 
-  [
-    { input: "190: 10 19", expected: true },
-    { input: "3267: 81 40 27", expected: true },
-    { input: "83: 17 5", expected: false },
-    { input: "156: 15 6", expected: true },
-    { input: "7290: 6 8 6 15", expected: true },
-    { input: "161011: 16 10 13", expected: false },
-    { input: "192: 17 8 14", expected: true },
-    { input: "21037: 9 7 18 13", expected: false },
-    { input: "292: 11 6 16 20", expected: true },
-  ].forEach(({ input, expected }) => {
-    it(`should return ${expected} from test plus input for ${JSON.stringify(input)}`, () => {
-      assert.deepStrictEqual(day.canBeValidPlus(input), expected);
+  describe("when concat operator is allowed", () => {
+    [
+      { input: "190: 10 19", expected: true },
+      { input: "3267: 81 40 27", expected: true },
+      { input: "83: 17 5", expected: false },
+      { input: "156: 15 6", expected: true },
+      { input: "7290: 6 8 6 15", expected: true },
+      { input: "161011: 16 10 13", expected: false },
+      { input: "192: 17 8 14", expected: true },
+      { input: "21037: 9 7 18 13", expected: false },
+      { input: "292: 11 6 16 20", expected: true },
+    ].forEach(({ input, expected }) => {
+      it(`should return ${expected} from test plus input for ${JSON.stringify(input)}`, () => {
+        assert.deepStrictEqual(day.canBeValid(input, true), expected);
+      });
     });
   });
 });
--- typescript/2024/day07.ts
+++ typescript/2024/day07.ts
@@ -6,64 +6,45 @@ export class Day07 implements Day {
 
   executePartOne(input: string[]): number {
     return input
-      .filter((x) => this.canBeValid(x))
-      .map((x) => Number(x.split(":")[0]))
+      .filter((calibrationEquation) =>
+        this.canBeValid(calibrationEquation, false),
+      )
+      .map((calibrationEquation) => Number(calibrationEquation.split(":")[0]))
       .reduce((a, b) => a + b);
   }
 
   executePartTwo(input: string[]): number {
     return input
-      .filter((x) => this.canBeValidPlus(x))
-      .map((x) => Number(x.split(":")[0]))
+      .filter((calibrationEquation) =>
+        this.canBeValid(calibrationEquation, true),
+      )
+      .map((calibrationEquation) => Number(calibrationEquation.split(":")[0]))
       .reduce((a, b) => a + b);
   }
 
-  canBeValid(input: string) {
+  canBeValid(input: string, concatOperatorAllowed: boolean) {
+    const numberOfOperators = concatOperatorAllowed ? 3 : 2;
     const [expectedSum, allNumbers] = input.split(": ");
     const numbers = allNumbers.split(" ").map(Number);
-    const amountOfTries = Math.pow(2, numbers.length - 1);
-    // console.log(amountOfTries, numbers);
+    const amountOfTries = Math.pow(numberOfOperators, numbers.length - 1);
     for (let i = 0; i < amountOfTries; i++) {
       let currentTotal = numbers[0];
       for (let j = 1; j < numbers.length; j++) {
-        const isAdd = Math.floor(i / Math.pow(2, j - 1)) % 2 === 0;
-        // console.log("hmm", i, j, isAdd, currentTotal);
-        if (isAdd) {
-          currentTotal += numbers[j];
-        } else {
-          currentTotal *= numbers[j];
+        switch (
+          Math.floor(i / Math.pow(numberOfOperators, j - 1)) % numberOfOperators
+        ) {
+          case 0:
+            currentTotal = currentTotal + numbers[j];
+            break;
+          case 1:
+            currentTotal = currentTotal * numbers[j];
+            break;
+          case 2:
+            currentTotal = Number(`${currentTotal}${numbers[j]}`);
+            break;
         }
       }
-      // console.log(i, currentTotal);
       if (currentTotal === Number(expectedSum)) {
-        // console.log("yay");
-        return true;
-      }
-    }
-    return false;
-  }
-
-  canBeValidPlus(input: string) {
-    const [expectedSum, allNumbers] = input.split(": ");
-    const numbers = allNumbers.split(" ").map(Number);
-    const amountOfTries = Math.pow(3, numbers.length - 1);
-    // console.log(amountOfTries, numbers);
-    for (let i = 0; i < amountOfTries; i++) {
-      let currentTotal = numbers[0];
-      for (let j = 1; j < numbers.length; j++) {
-        const operation = Math.floor(i / Math.pow(3, j - 1)) % 3;
-        // console.log("hmm", i, j, isAdd, currentTotal);
-        if (operation === 0) {
-          currentTotal += numbers[j];
-        } else if (operation === 1) {
-          currentTotal *= numbers[j];
-        } else {
-          currentTotal = Number(`${currentTotal}${numbers[j]}`);
-        }
-      }
-      // console.log(i, currentTotal);
-      if (currentTotal === Number(expectedSum)) {
-        // console.log("yay");
         return true;
       }
     }
