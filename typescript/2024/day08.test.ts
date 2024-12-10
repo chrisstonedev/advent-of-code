cebf711fe04deaeb375e409ebe9488cebea89347
Chris Stone <chrisstonedev@gmail.com>
2024-12-09 23:59:01 -0500

    Solved day 8 2024 in TypeScript

--- typescript/2024/day08.test.ts
+++ typescript/2024/day08.test.ts
@@ -2,10 +2,18 @@ import * as assert from "node:assert";
 import { describe, it } from "node:test";
 import { Day08 } from "./day08";
 
+function mapToObj(map: Map<string, number[][]>) {
+  const obj = Object.create(null);
+  for (const [k, v] of map) {
+    obj[k] = v;
+  }
+  return obj;
+}
+
 describe("Day 8", () => {
   const day = new Day08();
 
-  describe("when creating antinodes", () => {
+  describe("when creating the map for the input", () => {
     [
       {
         input: [
@@ -20,19 +28,16 @@ describe("Day 8", () => {
           "..........",
           "..........",
         ],
-        expected: [
-          "..........",
-          "...#......",
-          "..........",
-          "....a.....",
-          "..........",
-          ".....a....",
-          "..........",
-          "......#...",
-          "..........",
-          "..........",
-        ],
-        description: "2 antinodes for 2 antennas",
+        expected: new Map<string, number[][]>([
+          [
+            "a",
+            [
+              [3, 4],
+              [5, 5],
+            ],
+          ],
+        ]),
+        description: "2 antennas in 1 type",
       },
       {
         input: [
@@ -47,19 +52,18 @@ describe("Day 8", () => {
           "..........",
           "..........",
         ],
-        expected: [
-          "..........",
-          "...#......",
-          "#.........",
-          "....a.....",
-          "........a.",
-          ".....a....",
-          "..#.......",
-          "......A...",
-          "..........",
-          "..........",
-        ],
-        description: "3 antinodes for 3 antennas",
+        expected: new Map<string, number[][]>([
+          [
+            "a",
+            [
+              [3, 4],
+              [4, 8],
+              [5, 5],
+            ],
+          ],
+          ["A", [[7, 6]]],
+        ]),
+        description: "3 antennas for 1 type and 1 antenna for another",
       },
       {
         input: [
@@ -76,25 +80,207 @@ describe("Day 8", () => {
           "............",
           "............",
         ],
-        expected: [
-          "......#....#",
-          "...#....0...",
-          "....#0....#.",
-          "..#....0....",
-          "....0....#..",
-          ".#....A.....",
-          "...#........",
-          "#......#....",
-          "........A...",
-          ".........A..",
-          "..........#.",
-          "..........#.",
-        ],
-        description: "14 antinodes, including 1 overlap (oh no)",
+        expected: new Map<string, number[][]>([
+          [
+            "0",
+            [
+              [1, 8],
+              [2, 5],
+              [3, 7],
+              [4, 4],
+            ],
+          ],
+          [
+            "A",
+            [
+              [5, 6],
+              [8, 8],
+              [9, 9],
+            ],
+          ],
+        ]),
+        description: "all antennas from the test input",
       },
     ].forEach(({ input, expected, description }) => {
       it(`should return ${description}`, () => {
-        assert.deepStrictEqual(day.createAntinodes(input), expected);
+        const actual = day.evaluateInput(input);
+        const actualObject = mapToObj(actual);
+        const expectedObject = mapToObj(expected);
+        assert.deepEqual(actualObject, expectedObject);
+      });
+    });
+  });
+
+  describe("when finding antinodes from a map", () => {
+    [
+      {
+        input: new Map<string, number[][]>([
+          [
+            "a",
+            [
+              [3, 4],
+              [5, 5],
+            ],
+          ],
+        ]),
+        dataLength: 10,
+        expected: new Set(["1,3", "7,6"]),
+        description: "2 antinodes for 2 antennas",
+      },
+      {
+        input: new Map<string, number[][]>([
+          [
+            "a",
+            [
+              [3, 4],
+              [4, 8],
+              [5, 5],
+            ],
+          ],
+          ["A", [[7, 6]]],
+        ]),
+        dataLength: 10,
+        expected: new Set(["1,3", "2,0", "6,2", "7,6"]),
+        description: "3 antinodes for 3 antennas",
+      },
+      {
+        input: new Map<string, number[][]>([
+          [
+            "0",
+            [
+              [1, 8],
+              [2, 5],
+              [3, 7],
+              [4, 4],
+            ],
+          ],
+          [
+            "A",
+            [
+              [5, 6],
+              [8, 8],
+              [9, 9],
+            ],
+          ],
+        ]),
+        dataLength: 12,
+        expected: new Set([
+          "0,6",
+          "0,11",
+          "1,3",
+          "2,4",
+          "2,10",
+          "3,2",
+          "4,9",
+          "5,1",
+          "5,6",
+          "6,3",
+          "7,0",
+          "7,7",
+          "10,10",
+          "11,10",
+        ]),
+        description: "14 antinodes for the test input",
+      },
+    ].forEach(({ input, dataLength, expected, description }) => {
+      it(`should return ${description}`, () => {
+        assert.deepStrictEqual(day.findAntinodes(input, dataLength), expected);
+      });
+    });
+  });
+
+  describe("when finding resonant antinodes from a map", () => {
+    [
+      {
+        input: new Map<string, number[][]>([
+          [
+            "T",
+            [
+              [0, 0],
+              [1, 3],
+              [2, 1],
+            ],
+          ],
+        ]),
+        dataLength: 10,
+        expected: new Set([
+          "0,0",
+          "0,5",
+          "1,3",
+          "2,1",
+          "2,6",
+          "3,9",
+          "4,2",
+          "6,3",
+          "8,4",
+        ]),
+        description: "9 antinodes for 3 antennas",
+      },
+      {
+        input: new Map<string, number[][]>([
+          [
+            "0",
+            [
+              [1, 8],
+              [2, 5],
+              [3, 7],
+              [4, 4],
+            ],
+          ],
+          [
+            "A",
+            [
+              [5, 6],
+              [8, 8],
+              [9, 9],
+            ],
+          ],
+        ]),
+        dataLength: 12,
+        expected: new Set([
+          "0,0",
+          "0,1",
+          "0,11",
+          "0,6",
+          "1,1",
+          "1,3",
+          "1,8",
+          "2,10",
+          "2,2",
+          "2,4",
+          "2,5",
+          "3,2",
+          "3,3",
+          "3,7",
+          "4,4",
+          "4,9",
+          "5,1",
+          "5,11",
+          "5,5",
+          "5,6",
+          "6,3",
+          "6,6",
+          "7,0",
+          "7,5",
+          "7,7",
+          "8,2",
+          "8,8",
+          "9,4",
+          "9,9",
+          "10,1",
+          "10,10",
+          "11,10",
+          "11,11",
+          "11,3",
+        ]),
+        description: "34 antinodes from the test input",
+      },
+    ].forEach(({ input, dataLength, expected, description }) => {
+      it(`should return ${description}`, () => {
+        assert.deepStrictEqual(
+          day.findResonantAntinodes(input, dataLength),
+          expected,
+        );
       });
     });
   });
--- typescript/2024/day08.ts
+++ typescript/2024/day08.ts
@@ -2,18 +2,103 @@ import { Day } from "../program";
 
 export class Day08 implements Day {
   partOneTestAnswer = 14;
-  partTwoTestAnswer = 11387;
-  inProgress = true;
+  partTwoTestAnswer = 34;
 
   executePartOne(input: string[]): number {
-    return input.length;
+    const data = this.evaluateInput(input);
+    const set = this.findAntinodes(data, input.length);
+    return set.size;
   }
 
   executePartTwo(input: string[]): number {
-    return input.length;
+    const data = this.evaluateInput(input);
+    const set = this.findResonantAntinodes(data, input.length);
+    return set.size;
   }
 
-  createAntinodes(input: string[]) {
-    return input;
+  evaluateInput(input: string[]) {
+    const map = new Map<string, number[][]>();
+    for (let row = 0; row < input.length; row++) {
+      for (let col = 0; col < input[row].length; col++) {
+        if (input[row][col] !== ".") {
+          if (map.has(input[row][col])) {
+            map.get(input[row][col])!.push([row, col]);
+          } else {
+            map.set(input[row][col], [[row, col]]);
+          }
+        }
+      }
+    }
+    return map;
+  }
+
+  findAntinodes(input: Map<string, number[][]>, dataLength: number) {
+    const set = new Set<string>();
+    const antennaTypes = Array.from(input.keys()).filter(
+      (x) => input.get(x)!.length > 1,
+    );
+    for (const type of antennaTypes) {
+      const antennas = input.get(type)!;
+      for (let i = 0; i < antennas.length - 1; i++) {
+        for (let j = i + 1; j < antennas.length; j++) {
+          const [x1, y1] = antennas[i];
+          const [x2, y2] = antennas[j];
+          const xDelta = x2 - x1;
+          const yDelta = y2 - y1;
+          const antinode1 = [x1 - xDelta, y1 - yDelta];
+          const antinode2 = [x2 + xDelta, y2 + yDelta];
+          if (this.isValidAntinode(antinode1, dataLength)) {
+            set.add(`${antinode1[0]},${antinode1[1]}`);
+          }
+          if (this.isValidAntinode(antinode2, dataLength)) {
+            set.add(`${antinode2[0]},${antinode2[1]}`);
+          }
+        }
+      }
+    }
+    return set;
+  }
+
+  findResonantAntinodes(input: Map<string, number[][]>, dataLength: number) {
+    const set = new Set<string>();
+    const antennaTypes = Array.from(input.keys()).filter(
+      (x) => input.get(x)!.length > 1,
+    );
+    for (const type of antennaTypes) {
+      const antennas = input.get(type)!;
+      antennas
+        .map(([x, y]) => `${x},${y}`)
+        .forEach((antenna) => set.add(antenna));
+      for (let i = 0; i < antennas.length - 1; i++) {
+        for (let j = i + 1; j < antennas.length; j++) {
+          const [x1, y1] = antennas[i];
+          const [x2, y2] = antennas[j];
+          const xDelta = x2 - x1;
+          const yDelta = y2 - y1;
+          for (let mult = 1; ; mult++) {
+            const antinode1 = [x1 - xDelta * mult, y1 - yDelta * mult];
+            if (this.isValidAntinode(antinode1, dataLength)) {
+              set.add(`${antinode1[0]},${antinode1[1]}`);
+            } else {
+              break;
+            }
+          }
+          for (let mult = 1; ; mult++) {
+            const antinode2 = [x2 + xDelta * mult, y2 + yDelta * mult];
+            if (this.isValidAntinode(antinode2, dataLength)) {
+              set.add(`${antinode2[0]},${antinode2[1]}`);
+            } else {
+              break;
+            }
+          }
+        }
+      }
+    }
+    return set;
+  }
+
+  private isValidAntinode(antinode: number[], dataLength: number) {
+    const [x, y] = antinode;
+    return x >= 0 && y >= 0 && x < dataLength && y < dataLength;
   }
 }
