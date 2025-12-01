9eb07b8ee7a6567d9b7c447d7d67b47c279a0a7e
Chris Stone <chrisstonedev@gmail.com>
2025-12-01 00:48:45 -0500

    Day 1 of 2025 complete in Go, though only after rewriting function, I don't understand what I had wrong yet

--- go/aoc2025/day01.go
+++ go/aoc2025/day01.go
@@ -1,9 +1,133 @@
 package aoc2025
 
+import (
+	"fmt"
+	"log"
+	"strconv"
+	"strings"
+)
+
 func Part1(input string) int {
-	return len(input)
+	timesItGotToZero := 0
+	value := 50
+	for _, instruction := range strings.Split(input, "\n") {
+		if strings.HasPrefix(instruction, "L") {
+			amount := instruction[1:]
+			b, err := strconv.Atoi(amount)
+			if err != nil {
+				log.Panicf("bad number? %s", instruction)
+			}
+			value = value - b
+			for value < 0 {
+				value = 100 + value
+			}
+		} else if strings.HasPrefix(instruction, "R") {
+			amount := instruction[1:]
+			b, err := strconv.Atoi(amount)
+			if err != nil {
+				log.Panicf("bad number? %s", instruction)
+			}
+			value = value + b
+			for value > 99 {
+				value = value - 100
+			}
+		}
+		if value == 0 {
+			timesItGotToZero++
+		}
+	}
+	return timesItGotToZero
 }
 
 func Part2(input string) int {
-	return len(input)
+	timesItGotToZero := 0
+	value := 50
+	for _, instruction := range strings.Split(input, "\n") {
+		previousTimes := timesItGotToZero
+		direction := " up "
+		if strings.HasPrefix(instruction, "L") {
+			direction = "down"
+			if value == 0 {
+				value = 100
+			}
+			amount := instruction[1:]
+			b, err := strconv.Atoi(amount)
+			if err != nil {
+				log.Panicf("bad number? %s", instruction)
+			}
+			value = value - b
+			if value == 0 {
+				timesItGotToZero++
+			}
+			for value < 0 {
+				value = 100 + value
+				timesItGotToZero++
+			}
+		} else if strings.HasPrefix(instruction, "R") {
+			amount := instruction[1:]
+			b, err := strconv.Atoi(amount)
+			if err != nil {
+				log.Panicf("bad number? %s", instruction)
+			}
+			value = value + b
+			for value > 99 {
+				value = value - 100
+				timesItGotToZero++
+			}
+		}
+		increaseExplanation := "did not hit 0 this time"
+		if previousTimes != timesItGotToZero {
+			increaseExplanation = fmt.Sprintf("hit zero %d time(s), for a new total of %d", timesItGotToZero-previousTimes, timesItGotToZero)
+		}
+		fmt.Printf("%s: went %s to %d and %s\n", instruction, direction, value, increaseExplanation)
+	}
+	return timesItGotToZero
+}
+
+func Part2NextGen(input string) int {
+	timesItGotToZero := 0
+	value := 50
+	for _, instruction := range strings.Split(input, "\n") {
+		previousTimes := timesItGotToZero
+		direction := " up "
+		if strings.HasPrefix(instruction, "L") {
+			direction = "down"
+			if value == 0 {
+				value = 100
+			}
+			amount := instruction[1:]
+			b, err := strconv.Atoi(amount)
+			if err != nil {
+				log.Panicf("bad number? %s", instruction)
+			}
+			for i := 0; i < b; i++ {
+				value--
+				if value == 0 {
+					timesItGotToZero++
+				}
+				if value == -1 {
+					value = 99
+				}
+			}
+		} else if strings.HasPrefix(instruction, "R") {
+			amount := instruction[1:]
+			b, err := strconv.Atoi(amount)
+			if err != nil {
+				log.Panicf("bad number? %s", instruction)
+			}
+			for i := 0; i < b; i++ {
+				value++
+				if value == 100 {
+					timesItGotToZero++
+					value = 0
+				}
+			}
+		}
+		increaseExplanation := "did not hit 0 this time"
+		if previousTimes != timesItGotToZero {
+			increaseExplanation = fmt.Sprintf("hit zero %d time(s), for a new total of %d", timesItGotToZero-previousTimes, timesItGotToZero)
+		}
+		fmt.Printf("%s: went %s to %d and %s\n", instruction, direction, value, increaseExplanation)
+	}
+	return timesItGotToZero
 }
--- go/aoc2025/day01_test.go
+++ go/aoc2025/day01_test.go
@@ -1,9 +1,11 @@
 package aoc2025
 
 import (
+	"fmt"
+	"strings"
 	"testing"
 
-	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 
 	"aoc/utils"
 )
@@ -12,20 +14,58 @@ const date = "2025_01"
 
 func TestDay01Part1(t *testing.T) {
 	input := utils.ReadFileIntoString(date, "test")
-	assert.Equal(t, 54, Part1(input))
+	require.Equal(t, 3, Part1(input))
 }
 
+// it's not 281
 func TestDay01Part1Input(t *testing.T) {
 	input := utils.ReadFileIntoString(date, "input")
-	assert.Equal(t, 10496, Part1(input))
+	require.Equal(t, 1147, Part1(input))
 }
 
 func TestDay01Part2(t *testing.T) {
 	input := utils.ReadFileIntoString(date, "test")
-	assert.Equal(t, 54, Part2(input))
+	require.Equal(t, 6, Part2(input))
 }
 
 func TestDay01Part2Input(t *testing.T) {
 	input := utils.ReadFileIntoString(date, "input")
-	assert.Equal(t, 10496, Part2(input))
+	require.Equal(t, 6789, Part2(input))
+}
+
+func TestDay01Part2NextGen(t *testing.T) {
+	input := utils.ReadFileIntoString(date, "test")
+	require.Equal(t, 6, Part2NextGen(input))
+}
+
+func TestDay01Part2NextGenInput(t *testing.T) {
+	input := utils.ReadFileIntoString(date, "input")
+	require.Equal(t, 6789, Part2NextGen(input))
+}
+
+func TestPart2InSmallerParts(t *testing.T) {
+	tests := []struct {
+		input    []string
+		source   string
+		expected int
+	}{
+		{[]string{"L68"}, "test", 1},
+		{[]string{"L68", "L30"}, "test", 1},
+		{[]string{"L68", "L30", "R48"}, "test", 2},
+		{[]string{"L68", "L30", "R48", "L5"}, "test", 2},
+		{[]string{"L68", "L30", "R48", "L5", "R60"}, "test", 3},
+		{[]string{"L68", "L30", "R48", "L5", "R60", "L55"}, "test", 4},
+		{[]string{"L68", "L30", "R48", "L5", "R60", "L55", "L1"}, "test", 4},
+		{[]string{"L68", "L30", "R48", "L5", "R60", "L55", "L1", "L99"}, "test", 5},
+		{[]string{"L68", "L30", "R48", "L5", "R60", "L55", "L1", "L99", "R14"}, "test", 5},
+		{[]string{"L68", "L30", "R48", "L5", "R60", "L55", "L1", "L99", "R14", "L82"}, "test", 6},
+		{[]string{"L68", "L30", "R48", "L5", "R60", "L55", "L1", "L99", "R14", "L82", "R550"}, "fake (R)", 11},
+		{[]string{"L68", "L30", "R48", "L5", "R60", "L55", "L1", "L99", "R14", "L82", "L550"}, "fake (L)", 12},
+		{[]string{"R1000"}, "sample", 10},
+	}
+	for _, tt := range tests {
+		t.Run(fmt.Sprintf("Trying with only %d lines of %s input", len(tt.input), tt.source), func(t *testing.T) {
+			require.Equal(t, tt.expected, Part2(strings.Join(tt.input, "\n")))
+		})
+	}
 }
