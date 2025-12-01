d27b531d774248bb1c30e533c4707e7d29cd0627
Chris Stone <chrisstonedev@gmail.com>
2025-12-01 01:15:26 -0500

    Simplify code, parsing instruction into common function so we do not need conditional logic on the letter provided

--- go/aoc2025/day01.go
+++ go/aoc2025/day01.go
@@ -1,8 +1,6 @@
 package aoc2025
 
 import (
-	"fmt"
-	"log"
 	"strconv"
 	"strings"
 )
@@ -11,26 +9,13 @@ func Part1(input string) int {
 	timesItGotToZero := 0
 	value := 50
 	for _, instruction := range strings.Split(input, "\n") {
-		if strings.HasPrefix(instruction, "L") {
-			amount := instruction[1:]
-			b, err := strconv.Atoi(amount)
-			if err != nil {
-				log.Panicf("bad number? %s", instruction)
-			}
-			value = value - b
-			for value < 0 {
-				value = 100 + value
-			}
-		} else if strings.HasPrefix(instruction, "R") {
-			amount := instruction[1:]
-			b, err := strconv.Atoi(amount)
-			if err != nil {
-				log.Panicf("bad number? %s", instruction)
-			}
-			value = value + b
-			for value > 99 {
-				value = value - 100
-			}
+		amount := mustParseInstructionAmount(instruction)
+		value = value + amount
+		for value < 0 {
+			value = 100 + value
+		}
+		for value > 99 {
+			value = value - 100
 		}
 		if value == 0 {
 			timesItGotToZero++
@@ -43,82 +28,36 @@ func Part2(input string) int {
 	timesItGotToZero := 0
 	value := 50
 	for _, instruction := range strings.Split(input, "\n") {
-		previousTimes := timesItGotToZero
-		if strings.HasPrefix(instruction, "L") {
-			if value == 0 {
-				value = 100
-			}
-			amount := instruction[1:]
-			b, err := strconv.Atoi(amount)
-			if err != nil {
-				log.Panicf("bad number? %s", instruction)
-			}
-			value = value - b
-			for value < 0 {
-				value = 100 + value
-				timesItGotToZero++
-			}
-			if value == 0 {
-				timesItGotToZero++
-			}
-		} else if strings.HasPrefix(instruction, "R") {
-			amount := instruction[1:]
-			b, err := strconv.Atoi(amount)
-			if err != nil {
-				log.Panicf("bad number? %s", instruction)
-			}
-			value = value + b
-			for value > 99 {
-				value = value - 100
-				timesItGotToZero++
-			}
+		amount := mustParseInstructionAmount(instruction)
+		if amount < 0 && value == 0 {
+			value = 100
 		}
-		fmt.Printf("after %s, value is now %d\n", instruction, value)
-		for i := previousTimes + 1; i <= timesItGotToZero; i++ {
-			fmt.Printf("%d. %s\n", i, instruction)
+		value = value + amount
+		for value < 0 {
+			value = 100 + value
+			timesItGotToZero++
+		}
+		if value == 0 {
+			timesItGotToZero++
+		}
+		for value > 99 {
+			value = value - 100
+			timesItGotToZero++
 		}
 	}
 	return timesItGotToZero
 }
 
-func Part2NextGen(input string) int {
-	timesItGotToZero := 0
-	value := 50
-	for _, instruction := range strings.Split(input, "\n") {
-		if strings.HasPrefix(instruction, "L") {
-			if value == 0 {
-				value = 100
-			}
-			amount := instruction[1:]
-			b, err := strconv.Atoi(amount)
-			if err != nil {
-				log.Panicf("bad number? %s", instruction)
-			}
-			for i := 0; i < b; i++ {
-				value--
-				if value == 0 {
-					timesItGotToZero++
-					fmt.Printf("%d. %s\n", timesItGotToZero, instruction)
-				}
-				if value == -1 {
-					value = 99
-				}
-			}
-		} else if strings.HasPrefix(instruction, "R") {
-			amount := instruction[1:]
-			b, err := strconv.Atoi(amount)
-			if err != nil {
-				log.Panicf("bad number? %s", instruction)
-			}
-			for i := 0; i < b; i++ {
-				value++
-				if value == 100 {
-					timesItGotToZero++
-					fmt.Printf("%d. %s\n", timesItGotToZero, instruction)
-					value = 0
-				}
-			}
-		}
+func mustParseInstructionAmount(instruction string) int {
+	amount, err := strconv.Atoi(instruction[1:])
+	if err != nil {
+		panic("unable to parse what was expected to be a number")
 	}
-	return timesItGotToZero
+	if strings.HasPrefix(instruction, "L") {
+		return amount * -1
+	}
+	if strings.HasPrefix(instruction, "R") {
+		return amount
+	}
+	panic("instruction does not contain expected prefix")
 }
--- go/aoc2025/day01_test.go
+++ go/aoc2025/day01_test.go
@@ -17,7 +17,6 @@ func TestDay01Part1(t *testing.T) {
 	require.Equal(t, 3, Part1(input))
 }
 
-// it's not 281
 func TestDay01Part1Input(t *testing.T) {
 	input := utils.ReadFileIntoString(date, "input")
 	require.Equal(t, 1147, Part1(input))
@@ -31,16 +30,6 @@ func TestDay01Part2(t *testing.T) {
 func TestDay01Part2Input(t *testing.T) {
 	input := utils.ReadFileIntoString(date, "input")
 	require.Equal(t, 6789, Part2(input))
-}
-
-func TestDay01Part2NextGen(t *testing.T) {
-	input := utils.ReadFileIntoString(date, "test")
-	require.Equal(t, 6, Part2NextGen(input))
-}
-
-func TestDay01Part2NextGenInput(t *testing.T) {
-	input := utils.ReadFileIntoString(date, "input")
-	require.Equal(t, 6789, Part2NextGen(input))
 }
 
 func TestPart2InSmallerParts(t *testing.T) {
@@ -62,8 +51,7 @@ func TestPart2InSmallerParts(t *testing.T) {
 		{[]string{"L68", "L30", "R48", "L5", "R60", "L55", "L1", "L99", "R14", "L82", "R550"}, "fake (R)", 11},
 		{[]string{"L68", "L30", "R48", "L5", "R60", "L55", "L1", "L99", "R14", "L82", "L550"}, "fake (L)", 12},
 		{[]string{"R1000"}, "sample", 10},
-		{[]string{"L50", "R6", "L15", "L18", "R24", "R28", "L29", "L41", "L13", "R49", "R18", "R36", "L28", "L10", "R16", "R1", "L47", "R4", "R45", "R45", "L49", "R13", "L19", "R24", "R34", "R13", "L42", "L35", "L14", "R38", "R2", "R31", "L4", "R37", "L40", "L19", "L42", "R32", "L50", "R23", "L31", "R43", "L46", "R36", "R17", "L29", "L25", "R23", "L28", "L80", "R50", "R51", "R98", "R58", "R92", "L57", "R98", "R95", "L69", "L61", "L39", "L21", "L53", "R21", "R53", "L88", "L35", "R7", "L84", "L16", "L84", "R36", "L36", "R90", "R98", "L88", "L39", "R39", "L44", "L94", "R33", "L95", "R82", "R95", "R54", "R32", "R37", "R43", "L43", "L62", "L48", "L15", "L63", "R88", "R94", "R6", "L54", "R86", "L29", "R13", "R84", "L60", "L50", "R81", "L174", "L10", "L436", "R56", "R47", "L677", "R14"}, "input", 64},
-		{[]string{"L50", "R6", "L15", "L18", "R24", "R28", "L29", "L41", "L13", "R49", "R18", "R36", "L28", "L10", "R16", "R1", "L47", "R4", "R45", "R45", "L49", "R13", "L19", "R24", "R34", "R13", "L42", "L35", "L14", "R38", "R2", "R31", "L4", "R37", "L40", "L19", "L42", "R32", "L50", "R23", "L31", "R43", "L46", "R36", "R17", "L29", "L25", "R23", "L28", "L80", "R50", "R51", "R98", "R58", "R92", "L57", "R98", "R95", "L69", "L61", "L39", "L21", "L53", "R21", "R53", "L88", "L35", "R7", "L84", "L16", "L84", "R36", "L36", "R90", "R98", "L88", "L39", "R39", "L44", "L94", "R33", "L95", "R82", "R95", "R54", "R32", "R37", "R43", "L43", "L62", "L48", "L15", "L63", "R88", "R94", "R6", "L54", "R86", "L29", "R13", "R84", "L60", "L50", "R81", "L174", "L10", "L436", "R56", "R47", "L677", "R14", "L391"}, "input", 68},
+		{[]string{"R56", "L106"}, "sample", 3},
 	}
 	for _, tt := range tests {
 		t.Run(fmt.Sprintf("Trying with only %d lines of %s input", len(tt.input), tt.source), func(t *testing.T) {
