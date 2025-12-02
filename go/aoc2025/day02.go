cc4eddcfe6ff4e80cd4a6de43f4065d8f9f9a47e
Chris Stone <chrisstonedev@gmail.com>
2025-12-02 00:27:18 -0500

    Part 2 still takes 9 seconds, and now Part 1 takes 11 seconds instead of less than 1, but at least we've deduplicated code, and maybe we can take lessons from the old part 1 and apply them to this common branch

--- go/aoc2025/day02.go
+++ go/aoc2025/day02.go
@@ -8,25 +8,19 @@ import (
 )
 
 func Day02Part1(input string) int {
-	splits := strings.Split(input, ",")
-	invalid := 0
-	for _, split := range splits {
-		s2 := strings.Split(split, "-")
-		num1, _ := strconv.Atoi(s2[0])
-		num2, _ := strconv.Atoi(s2[1])
-		for i := num1; i <= num2; i++ {
-			text := strconv.Itoa(i)
-			if len(text)%2 == 0 && text[:len(text)/2] == text[len(text)/2:] {
-				invalid += i
-			}
-		}
-	}
-	return invalid
+	return funcName(strings.Split(input, ","), 2)
 }
 
 func Day02Part2(input string) int {
-	splits := strings.Split(input, ",")
+	return funcName(strings.Split(input, ","), 0)
+}
+
+func funcName(splits []string, maxRepeats int) int {
 	invalid := 0
+	maxRepeatRegExpFormat := fmt.Sprintf("{%d}", maxRepeats)
+	if maxRepeats == 0 {
+		maxRepeatRegExpFormat = "+"
+	}
 	for _, split := range splits {
 		s2 := strings.Split(split, "-")
 		num1, _ := strconv.Atoi(s2[0])
@@ -35,7 +29,7 @@ func Day02Part2(input string) int {
 			text := strconv.Itoa(i)
 			for j := len(text) / 2; j >= 1; j-- {
 				segment := text[:j]
-				r := regexp.MustCompile(fmt.Sprintf(`^(%s)+$`, segment))
+				r := regexp.MustCompile(fmt.Sprintf(`^(%s)%s$`, segment, maxRepeatRegExpFormat))
 				if r.MatchString(text) {
 					invalid += i
 					break
