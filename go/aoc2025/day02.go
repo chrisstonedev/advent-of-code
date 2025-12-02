26c3aedc847f584358111cb43570a5dc1539db7d
Chris Stone <chrisstonedev@gmail.com>
2025-12-02 00:23:28 -0500

    Completed day 2 2025 in Go, though getting the answer for part 2 takes 9 seconds

--- go/aoc2025/day02.go
+++ go/aoc2025/day02.go
@@ -1,13 +1,47 @@
 package aoc2025
 
 import (
+	"fmt"
+	"regexp"
+	"strconv"
 	"strings"
 )
 
 func Day02Part1(input string) int {
-	return len(strings.Split(input, "\n"))
+	splits := strings.Split(input, ",")
+	invalid := 0
+	for _, split := range splits {
+		s2 := strings.Split(split, "-")
+		num1, _ := strconv.Atoi(s2[0])
+		num2, _ := strconv.Atoi(s2[1])
+		for i := num1; i <= num2; i++ {
+			text := strconv.Itoa(i)
+			if len(text)%2 == 0 && text[:len(text)/2] == text[len(text)/2:] {
+				invalid += i
+			}
+		}
+	}
+	return invalid
 }
 
 func Day02Part2(input string) int {
-	return len(strings.Split(input, "\n"))
+	splits := strings.Split(input, ",")
+	invalid := 0
+	for _, split := range splits {
+		s2 := strings.Split(split, "-")
+		num1, _ := strconv.Atoi(s2[0])
+		num2, _ := strconv.Atoi(s2[1])
+		for i := num1; i <= num2; i++ {
+			text := strconv.Itoa(i)
+			for j := len(text) / 2; j >= 1; j-- {
+				segment := text[:j]
+				r := regexp.MustCompile(fmt.Sprintf(`^(%s)+$`, segment))
+				if r.MatchString(text) {
+					invalid += i
+					break
+				}
+			}
+		}
+	}
+	return invalid
 }
--- go/aoc2025/day02_test.go
+++ go/aoc2025/day02_test.go
@@ -12,33 +12,64 @@ const day02 = "2025_02"
 
 func TestDay02Part1(t *testing.T) {
 	input := utils.ReadFileIntoString(day02, "test")
-	require.Equal(t, 3, Day01Part1(input))
+	require.Equal(t, 1227775554, Day02Part1(input))
 }
 
 func TestDay02Part1Input(t *testing.T) {
 	input := utils.ReadFileIntoString(day02, "input")
-	require.Equal(t, 1147, Day01Part1(input))
+	require.Equal(t, 13919717792, Day02Part1(input))
 }
 
 func TestDay02Part2(t *testing.T) {
 	input := utils.ReadFileIntoString(day02, "test")
-	require.Equal(t, 6, Day01Part2(input))
+	require.Equal(t, 4174379265, Day02Part2(input))
 }
 
 func TestDay02Part2Input(t *testing.T) {
 	input := utils.ReadFileIntoString(day02, "input")
-	require.Equal(t, 6789, Day01Part2(input))
+	require.Equal(t, 14582313461, Day02Part2(input))
 }
 
 func TestDay02InSmallerParts(t *testing.T) {
 	tests := []struct {
-		name string
+		name     string
+		input    string
+		expected int
 	}{
-		{"Sample table test"},
+		{"sample1", "11-22", 33},
+		{"sample2", "95-115", 99},
+		{"sample3", "998-1012", 1010},
+		{"sample4", "1188511880-1188511890", 1188511885},
+		{"sample5", "222220-222224", 222222},
+		{"sample6", "1698522-1698528", 0},
+		{"sample7", "446443-446449", 446446},
+		{"sample8", "38593856-38593862", 38593859},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			require.Fail(t, "No test implemented yet")
+			require.Equal(t, tt.expected, Day02Part1(tt.input))
+		})
+	}
+}
+
+func TestDay02Part2InSmallerParts(t *testing.T) {
+	tests := []struct {
+		name     string
+		input    string
+		expected int
+	}{
+		{"sample1", "11-22", 33},
+		{"sample2", "95-115", 210},
+		{"sample3", "998-1012", 2009},
+		{"sample4", "1188511880-1188511890", 1188511885},
+		{"sample5", "222220-222224", 222222},
+		{"sample6", "1698522-1698528", 0},
+		{"sample7", "446443-446449", 446446},
+		{"sample8", "38593856-38593862", 38593859},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			require.Equal(t, tt.expected, Day02Part2(tt.input))
 		})
 	}
 }
