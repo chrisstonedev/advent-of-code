e50260cba5e4b0227fa783a1b779b99f1c6a3f21
Chris Stone <chrisstonedev@gmail.com>
2025-12-02 07:46:25 -0500

    Got rid of regexp to improve performance

--- go/aoc2025/day02.go
+++ go/aoc2025/day02.go
@@ -1,8 +1,6 @@
 package aoc2025
 
 import (
-	"fmt"
-	"regexp"
 	"strconv"
 	"strings"
 )
@@ -17,31 +15,39 @@ func Day02Part2(input string) int {
 
 func sumAllInvalidIDs(productIDRanges []string, maxRepeats int) int {
 	sum := 0
-	maxRepeatRegExpFormat := fmt.Sprintf("{%d}", maxRepeats)
-	if maxRepeats == 0 {
-		maxRepeatRegExpFormat = "+"
-	}
 	for _, productIDRange := range productIDRanges {
 		s2 := strings.Split(productIDRange, "-")
 		rangeStart, _ := strconv.Atoi(s2[0])
 		rangeEnd, _ := strconv.Atoi(s2[1])
 		for productID := rangeStart; productID <= rangeEnd; productID++ {
 			text := strconv.Itoa(productID)
-			for j := len(text) / 2; j >= 1; j-- {
-				segment := text[:j]
-				if len(text)%len(segment) != 0 {
-					continue
-				}
-				r := regexp.MustCompile(fmt.Sprintf(`^(%s)%s$`, segment, maxRepeatRegExpFormat))
-				if r.MatchString(text) {
+			if maxRepeats == 2 && len(text)%2 == 1 {
+				continue
+			}
+			for segmentLength := len(text) / 2; segmentLength >= 1; segmentLength-- {
+				if isTextARepeatingPattern(text, segmentLength) {
 					sum += productID
 					break
 				}
-				if j == 2 {
+				if maxRepeats == 2 {
 					break
 				}
 			}
 		}
 	}
 	return sum
+}
+
+func isTextARepeatingPattern(text string, segmentLength int) bool {
+	if len(text)%segmentLength != 0 {
+		return false
+	}
+	firstSegment := text[:segmentLength]
+	for i := segmentLength; i < len(text); i += segmentLength {
+		newTestSegment := text[i : i+segmentLength]
+		if firstSegment != newTestSegment {
+			return false
+		}
+	}
+	return true
 }
