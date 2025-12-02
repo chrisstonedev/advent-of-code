4054f0bf1903619a5e020daef66fbbaabbb732dc
Chris Stone <chrisstonedev@gmail.com>
2025-12-02 00:29:37 -0500

    Refactor variable names

--- go/aoc2025/day02.go
+++ go/aoc2025/day02.go
@@ -8,34 +8,34 @@ import (
 )
 
 func Day02Part1(input string) int {
-	return funcName(strings.Split(input, ","), 2)
+	return sumAllInvalidIDs(strings.Split(input, ","), 2)
 }
 
 func Day02Part2(input string) int {
-	return funcName(strings.Split(input, ","), 0)
+	return sumAllInvalidIDs(strings.Split(input, ","), 0)
 }
 
-func funcName(splits []string, maxRepeats int) int {
-	invalid := 0
+func sumAllInvalidIDs(productIDRanges []string, maxRepeats int) int {
+	sum := 0
 	maxRepeatRegExpFormat := fmt.Sprintf("{%d}", maxRepeats)
 	if maxRepeats == 0 {
 		maxRepeatRegExpFormat = "+"
 	}
-	for _, split := range splits {
-		s2 := strings.Split(split, "-")
-		num1, _ := strconv.Atoi(s2[0])
-		num2, _ := strconv.Atoi(s2[1])
-		for i := num1; i <= num2; i++ {
-			text := strconv.Itoa(i)
+	for _, productIDRange := range productIDRanges {
+		s2 := strings.Split(productIDRange, "-")
+		rangeStart, _ := strconv.Atoi(s2[0])
+		rangeEnd, _ := strconv.Atoi(s2[1])
+		for productID := rangeStart; productID <= rangeEnd; productID++ {
+			text := strconv.Itoa(productID)
 			for j := len(text) / 2; j >= 1; j-- {
 				segment := text[:j]
 				r := regexp.MustCompile(fmt.Sprintf(`^(%s)%s$`, segment, maxRepeatRegExpFormat))
 				if r.MatchString(text) {
-					invalid += i
+					sum += productID
 					break
 				}
 			}
 		}
 	}
-	return invalid
+	return sum
 }
