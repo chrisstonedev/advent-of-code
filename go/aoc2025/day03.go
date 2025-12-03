d02134ce29df903423ac553c9286b6703083936f
Chris Stone <chrisstonedev@gmail.com>
2025-12-03 08:35:10 -0500

    Refactor variable names

--- go/aoc2025/day03.go
+++ go/aoc2025/day03.go
@@ -2,13 +2,14 @@ package aoc2025
 
 import (
 	"fmt"
+	"log"
 	"strconv"
 	"strings"
 )
 
 func Day03Part1(input string) int {
 	allStrings := strings.Split(input, "\n")
-	value := 0
+	totalOutputJoltage := 0
 	for _, thing := range allStrings {
 		startNumber := "0"[0]
 		endNumber := "0"[0]
@@ -24,10 +25,14 @@ func Day03Part1(input string) int {
 				endNumber = thing[i]
 			}
 		}
-		b, _ := strconv.Atoi(fmt.Sprintf("%c%c", startNumber, endNumber))
-		value += b
+		joltage := fmt.Sprintf("%c%c", startNumber, endNumber)
+		joltageValue, err := strconv.Atoi(joltage)
+		if err != nil {
+			log.Panicf("unable to parse %s as number, %v", joltage, err)
+		}
+		totalOutputJoltage += joltageValue
 	}
-	return value
+	return totalOutputJoltage
 }
 
 func Day03Part2(input string) int {
@@ -36,21 +41,24 @@ func Day03Part2(input string) int {
 	for _, thing := range allStrings {
 		newThing := thing
 		for len(newThing) > 12 {
-			newThing = chopOffALetter(newThing)
+			newThing = highestPossibleJoltageWhenDroppingADigit(newThing)
 		}
-		b, _ := strconv.Atoi(newThing)
+		b, err := strconv.Atoi(newThing)
+		if err != nil {
+			log.Panicf("unable to parse %s as number, %v", newThing, err)
+		}
 		value += b
 	}
 	return value
 }
 
-func chopOffALetter(thing string) string {
-	highestTempThing := thing[1:]
-	for i := 1; i < len(thing); i++ {
-		testThing := thing[:i] + thing[i+1:]
-		if testThing > highestTempThing {
-			highestTempThing = testThing
+func highestPossibleJoltageWhenDroppingADigit(currentJoltage string) string {
+	largestResultingJoltage := currentJoltage[1:]
+	for i := 1; i < len(currentJoltage); i++ {
+		testJoltage := currentJoltage[:i] + currentJoltage[i+1:]
+		if testJoltage > largestResultingJoltage {
+			largestResultingJoltage = testJoltage
 		}
 	}
-	return highestTempThing
+	return largestResultingJoltage
 }
