9f358346e1978e1bb31448c7367bb234f3f83462
Chris Stone <chrisstonedev@gmail.com>
2025-12-09 01:27:55 -0500

    Implented Day 9 2025 in Go, but part 2 is taking too long

---
+++ go/aoc2025/day09.go
@@ -0,0 +1,156 @@
+package aoc2025
+
+import (
+	"fmt"
+	"slices"
+	"sort"
+	"strconv"
+	"strings"
+)
+
+func Day09Part1(input string) int {
+	lines := strings.Split(input, "\n")
+	area, _ := pairWithBiggestArea(lines)
+	return area
+}
+
+func Day09Part2(input string) int {
+	lines := strings.Split(input, "\n")
+	pairs := pairsWithBiggestArea(lines)
+	fmt.Println("got all pairs")
+	perimeter := getPerimeter(lines)
+	fmt.Println("got perimeter")
+	for _, pair := range pairs {
+		if isInside(pair, perimeter) {
+			return pair.Area
+		}
+	}
+	return 0
+}
+
+func isInside(pair SomeKindOfType, perimeter [][]int) bool {
+	for x := min(pair.X1, pair.X2) + 1; x < max(pair.X1, pair.X2); x++ {
+		for y := min(pair.Y1, pair.Y2) + 1; y < max(pair.Y1, pair.Y2); y++ {
+			if slices.IndexFunc(perimeter, func(e []int) bool {
+				return e[0] == x && e[1] == y
+			}) >= 0 {
+				continue
+			}
+			if y == 0 {
+				return false
+			}
+			edges := 0
+			for testY := y - 1; testY >= 0; testY-- {
+				if slices.IndexFunc(perimeter, func(e []int) bool {
+					return e[0] == x && e[1] == testY
+				}) >= 0 {
+					edges++
+				}
+			}
+			if edges%2 == 0 {
+				return false
+			}
+		}
+	}
+	return true
+}
+
+func getPerimeter(lines []string) [][]int {
+	pointsThatMakeUpThePerimeter := [][]int{lineToI(lines[0])}
+	for i := 1; i <= len(lines); i++ {
+		index := i % len(lines)
+		lastPoint := pointsThatMakeUpThePerimeter[len(pointsThatMakeUpThePerimeter)-1]
+		newPoint := lineToI(lines[index])
+		if lastPoint[0] == newPoint[0] {
+			if lastPoint[1] < newPoint[1] {
+				for j := lastPoint[1] + 1; j < newPoint[1]; j++ {
+					pointsThatMakeUpThePerimeter = append(pointsThatMakeUpThePerimeter, []int{newPoint[0], j})
+				}
+			} else {
+				for j := lastPoint[1] - 1; j > newPoint[1]; j-- {
+					pointsThatMakeUpThePerimeter = append(pointsThatMakeUpThePerimeter, []int{newPoint[0], j})
+				}
+			}
+		} else {
+			if lastPoint[0] < newPoint[0] {
+				for j := lastPoint[0] + 1; j < newPoint[0]; j++ {
+					pointsThatMakeUpThePerimeter = append(pointsThatMakeUpThePerimeter, []int{j, newPoint[1]})
+				}
+			} else {
+				for j := lastPoint[0] - 1; j > newPoint[0]; j-- {
+					pointsThatMakeUpThePerimeter = append(pointsThatMakeUpThePerimeter, []int{j, newPoint[1]})
+				}
+			}
+		}
+		if index != 0 {
+			pointsThatMakeUpThePerimeter = append(pointsThatMakeUpThePerimeter, newPoint)
+		}
+	}
+	return pointsThatMakeUpThePerimeter
+}
+
+func lineToI(s string) []int {
+	point := strings.Split(s, ",")
+	x, _ := strconv.Atoi(point[0])
+	y, _ := strconv.Atoi(point[1])
+	return []int{x, y}
+}
+
+func pairWithBiggestArea(input []string) (int, []string) {
+	area := 0
+	var pair []string
+	for i := 0; i < len(input)-1; i++ {
+		for j := i + 1; j < len(input); j++ {
+			point1 := strings.Split(input[i], ",")
+			point2 := strings.Split(input[j], ",")
+			x1, _ := strconv.Atoi(point1[0])
+			y1, _ := strconv.Atoi(point1[1])
+			x2, _ := strconv.Atoi(point2[0])
+			y2, _ := strconv.Atoi(point2[1])
+			xDiff := max(x1, x2) - min(x1, x2) + 1
+			yDiff := max(y1, y2) - min(y1, y2) + 1
+			newArea := xDiff * yDiff
+			if newArea > area {
+				area = newArea
+				pair = []string{input[i], input[j]}
+			}
+		}
+	}
+	return area, pair
+}
+
+type SomeKindOfType struct {
+	Area int
+	X1   int
+	Y1   int
+	X2   int
+	Y2   int
+}
+
+func pairsWithBiggestArea(input []string) []SomeKindOfType {
+	var pairs []SomeKindOfType
+	for i := 0; i < len(input)-1; i++ {
+		for j := i + 1; j < len(input); j++ {
+			point1 := strings.Split(input[i], ",")
+			point2 := strings.Split(input[j], ",")
+			x1, _ := strconv.Atoi(point1[0])
+			y1, _ := strconv.Atoi(point1[1])
+			x2, _ := strconv.Atoi(point2[0])
+			y2, _ := strconv.Atoi(point2[1])
+			xDiff := max(x1, x2) - min(x1, x2) + 1
+			yDiff := max(y1, y2) - min(y1, y2) + 1
+			area := xDiff * yDiff
+			pairs = append(pairs, SomeKindOfType{
+				Area: area,
+				X1:   x1,
+				Y1:   y1,
+				X2:   x2,
+				Y2:   y2,
+			})
+		}
+	}
+	sort.Slice(pairs, func(i, j int) bool {
+		return pairs[i].Area > pairs[j].Area
+	})
+	return pairs
+}
---
+++ go/aoc2025/day09_test.go
@@ -0,0 +1,88 @@
+package aoc2025
+
+import (
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/chrisstonedev/advent-of-code/go/utils"
+)
+
+const day09 = "2025_09"
+
+func TestDay09Part1(t *testing.T) {
+	input := utils.ReadFileIntoString(day09, "test")
+	require.Equal(t, 50, Day09Part1(input))
+}
+
+func TestWhatPairHasBiggestArea(t *testing.T) {
+	input := strings.Split(utils.ReadFileIntoString(day09, "test"), "\n")
+	area, pair := pairWithBiggestArea(input)
+	require.Equal(t, 50, area)
+	require.Equal(t, []string{"11,1", "2,5"}, pair)
+}
+
+func TestWhatPairsHaveBiggestArea(t *testing.T) {
+	input := strings.Split(utils.ReadFileIntoString(day09, "test"), "\n")
+	pairs := pairsWithBiggestArea(input)
+	require.Equal(t, SomeKindOfType{
+		Area: 50,
+		X1:   11,
+		Y1:   1,
+		X2:   2,
+		Y2:   5,
+	}, pairs[0])
+}
+
+func TestDay09Part1Input(t *testing.T) {
+	input := utils.ReadFileIntoString(day09, "input")
+	require.Equal(t, 4746238001, Day09Part1(input))
+}
+
+func TestDay09Part2(t *testing.T) {
+	input := utils.ReadFileIntoString(day09, "test")
+	require.Equal(t, 24, Day09Part2(input))
+}
+
+func TestDay09Part2Input(t *testing.T) {
+	input := utils.ReadFileIntoString(day09, "input")
+	require.Equal(t, 36045012, Day09Part2(input))
+}
+
+func TestGetPerimeter(t *testing.T) {
+	input := strings.Split(utils.ReadFileIntoString(day09, "test"), "\n")
+	perimeter := getPerimeter(input)
+	require.Equal(t, [][]int{
+		{7, 1},
+		{8, 1},
+		{9, 1},
+		{10, 1},
+		{11, 1},
+		{11, 2},
+		{11, 3},
+		{11, 4},
+		{11, 5},
+		{11, 6},
+		{11, 7},
+		{10, 7},
+		{9, 7},
+		{9, 6},
+		{9, 5},
+		{8, 5},
+		{7, 5},
+		{6, 5},
+		{5, 5},
+		{4, 5},
+		{3, 5},
+		{2, 5},
+		{2, 4},
+		{2, 3},
+		{3, 3},
+		{4, 3},
+		{5, 3},
+		{6, 3},
+		{7, 3},
+		{7, 2},
+	}, perimeter)
+}
